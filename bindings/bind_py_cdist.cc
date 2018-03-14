d6257e45f7ec648ad6096d4a63f6c4fa1a6da466
fede <federico.giberti@gmail.com>
2018-03-15 00:11:37 +0100

    Headers in cc files. Adding more comms and docs tomorrow

--- bindings/bind_py_cdist.cc
+++ bindings/bind_py_cdist.cc
@@ -1,3 +1,34 @@
+/**
+ * @file   bind_py_cdist.cc
+ *
+ * @author Federico Giberti <federico.giberti@epfl.ch>
+ *
+ * @date   14 Mar 2018
+ *
+ * @brief  File for binding the function cdist to a python object
+ *
+ * Copyright © 2017 Felix Musil
+ *
+ * Proteus is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3, or (at
+ * your option) any later version.
+ *
+ * Proteus is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Emacs; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
+
+
+
 #include <pybind11/pybind11.h>
 #include "cdist.h"
 
--- src/basic_types.h
+++ src/basic_types.h
@@ -1,4 +1,35 @@
+/**
+ * @file   basic_types.h
+ *
+ * @author Federico Giberti <federico.giberti@epfl.ch>
+ *
+ * @date   14 Mar 2018
+ *
+ * @brief  Implementation of base-type objects for Proteus
+ *
+ * Copyright © 2017 Felix Musil
+ *
+ * Proteus is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3, or (at
+ * your option) any later version.
+ *
+ * Proteus is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Emacs; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
 #include <Eigen/Dense>
 
+/**
+ * Dynamically allocated matrix of arbitrary dimension.
+ */
 Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Matrix;
 
