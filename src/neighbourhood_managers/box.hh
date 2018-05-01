a1a7d1caf1b31c80f30c9795435d40bab765f0ea
musil <felix.musil@epfl.ch>
2018-05-02 00:13:50 +0200

    need to make a boxes class

--- src/neighbourhood_managers/box.hh
+++ src/neighbourhood_managers/box.hh
@@ -45,7 +45,98 @@
 
 namespace proteus {
 
-  
+  template<class ManagerImplementation>
+  class Box {
+  public:
+    using AtomRef_t = typename ManagerImplementation::AtomRef_t;
+    //! Default constructor
+    Box() = default;
+
+    //! constructor
+    Box(NeighbourhoodManagerBase<ManagerImplementation> & manager, const Eigen::Ref<const Vec3i_t> coord, 
+                const std::array<std::array<Dim_t, 3>,2>& neigh_bounds, const Eigen::Ref<const Vec3i_t> nbins_c)
+                :manager{manager}
+    { 
+      //! warning: works only with negative a if |a| < b
+      std::function<void (int,int,std::array<int,2>)> branchless_div_mod = [](int a, int b,std::array<int,2> d) {d = {(a+b)/b,(a+b)%b};};
+      
+      this->coordinates = coord;
+      Vec3i_t shift,neighbour_bin_idx_c,neighbour_bin_shift;
+      std::array<int,2> div_mod;
+      int bin_id{0};
+      for (int dx{neigh_bounds[0][0]}; dx <= neigh_bounds[1][0]; ++dx){
+        for (int dy{neigh_bounds[0][1]}; dy <= neigh_bounds[1][1]; ++dy){
+          for (int dz{neigh_bounds[0][2]}; dz <= neigh_bounds[1][2]; ++dz){
+            shift << dx,dy,dz;
+            
+            for (int ii{0};ii<3;++ii){
+              branchless_div_mod(coord(ii)+shift(ii),nbins_c(ii),div_mod);
+              neighbour_bin_idx_c[ii] = div_mod[1];
+              neighbour_bin_shift[ii] = div_mod[0];
+            }
+
+            bin_id = internal::mult2lin(neighbour_bin_idx_c,nbins_c);
+            this->neighbour_bin_ids.push_back(bin_id);
+            this->neighbour_bin_shift.push_back(neighbour_bin_shift);
+            
+          }
+        }
+      }
+    };
+    //! copy constructor
+    Box(const Box & other) = default;
+    //! assignment operator
+    Box & operator=(const Box & other) = default;
+    virtual ~Box() = default;
+
+    inline void push_center_back(const int& id){
+      this->centers.push_back(AtomRef_t(this->manager,id));
+    }
+
+    
+    inline size_t get_number_of_centers(){
+      return this->centers.size();
+    }
+    /*
+    inline size_t get_number_of_neighbour(){
+      int Nneighbour{0};
+      for (auto bin_id : this->neighbour_bin_ids){
+        Nneighbour += this->manager.get_nb_of_center_in_box(bin_id);
+      }
+      return Nneighbour;
+    }*/
+
+    inline size_t get_number_of_neighbour(){
+      return this->neighbour_ids.size();
+    }
+
+    inline  int get_neighbour_index(int j_index){
+      return this->neighbour_bin_ids[j_index].get_index();
+    }
+
+    inline  std::vector<AtomRef_t> get_centers(){
+      return this->centers;
+    }
+    void set_neighbour_ids(){
+      for (auto bin_id:this->neighbour_bin_ids){
+        /*
+        size_t Ncenter = this->manager.get_nb_of_center_in_box(bin_id);
+        for (auto center : centers){
+          neighbour_ids.push_back(AtomRef_t(this->manager,center.get_index()));
+        }
+        */
+      }
+    }
+
+  protected:
+    Vec3i_t coordinates;
+    std::vector<AtomRef_t> centers;
+    NeighbourhoodManagerBase<ManagerImplementation> & manager;
+    std::vector<Vec3i_t,Eigen::aligned_allocator<Vec3i_t>> neighbour_bin_shift;
+    std::vector<Dim_t> neighbour_bin_ids;
+    // TODO replace neighbour_ids by an iterator that iterates over the centers in the neighbouring boxes
+    std::vector<AtomRef_t> neighbour_ids; 
+  };
 
 } //proteus
 
--- src/neighbourhood_managers/neighbourhood_manager_cell.hh
+++ src/neighbourhood_managers/neighbourhood_manager_cell.hh
@@ -31,15 +31,15 @@
 
 #include "neighbourhood_managers/neighbourhood_manager_base.hh"
 #include "neighbourhood_managers/box.hh"
-#include "neighbourhood_managers/field.hh"
-#include "lattice.hh"
-#include "basic_types.h"
 #include <Eigen/Dense>
 #include <Eigen/StdVector>
 #include <stdexcept>
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include "lattice.hh"
+#include "basic_types.h"
+#include "neighbourhood_managers/field.hh"
 #include <map>
 
 using namespace std;
@@ -88,7 +88,6 @@ namespace proteus {
     // required for the construction of vectors, etc
     constexpr static int dim() {return traits::Dim;}
 
-    class Box;
 
     // return position vector
     inline Vector_ref get_position(const AtomRef_t& atom) {
@@ -270,99 +269,7 @@ namespace proteus {
   get_offset_impl<1, 2>(const ClusterRef_t<1, 2>& cluster) const {
     return cluster.get_atoms().back().get_index();
   }
-  /* ---------------------------------------------------------------------- */
-  
-  class NeighbourhoodManagerCell::Box {
-  public:
-    using AtomRef_t = typename NeighbourhoodManagerCell::AtomRef_t;
-    //! Default constructor
-    Box() = default;
 
-    //! constructor
-    Box(NeighbourhoodManagerBase<NeighbourhoodManagerCell> & manager, const Eigen::Ref<const Vec3i_t> coord, 
-                const std::array<std::array<Dim_t, 3>,2>& neigh_bounds, const Eigen::Ref<const Vec3i_t> nbins_c)
-                :manager{manager}
-    { 
-      //! warning: works only with negative a if |a| < b
-      std::function<void (int,int,std::array<int,2>)> branchless_div_mod = [](int a, int b,std::array<int,2> d) {d = {(a+b)/b,(a+b)%b};};
-      
-      this->coordinates = coord;
-      Vec3i_t shift,neighbour_bin_idx_c,neighbour_bin_shift;
-      std::array<int,2> div_mod;
-      int bin_id{0};
-      for (int dx{neigh_bounds[0][0]}; dx <= neigh_bounds[1][0]; ++dx){
-        for (int dy{neigh_bounds[0][1]}; dy <= neigh_bounds[1][1]; ++dy){
-          for (int dz{neigh_bounds[0][2]}; dz <= neigh_bounds[1][2]; ++dz){
-            shift << dx,dy,dz;
-            
-            for (int ii{0};ii<3;++ii){
-              branchless_div_mod(coord(ii)+shift(ii),nbins_c(ii),div_mod);
-              neighbour_bin_idx_c[ii] = div_mod[1];
-              neighbour_bin_shift[ii] = div_mod[0];
-            }
-
-            bin_id = internal::mult2lin(neighbour_bin_idx_c,nbins_c);
-            this->neighbour_bin_ids.push_back(bin_id);
-            this->neighbour_bin_shift.push_back(neighbour_bin_shift);
-            
-          }
-        }
-      }
-    };
-    //! copy constructor
-    Box(const Box & other) = default;
-    //! assignment operator
-    Box & operator=(const Box & other) = default;
-    virtual ~Box() = default;
-
-    inline void push_center_back(const int& id){
-      this->centers.push_back(AtomRef_t(this->manager,id));
-    }
-
-    
-    inline size_t get_number_of_centers(){
-      return this->centers.size();
-    }
-    /*
-    inline size_t get_number_of_neighbour(){
-      int Nneighbour{0};
-      for (auto bin_id : this->neighbour_bin_ids){
-        Nneighbour += this->manager.get_nb_of_center_in_box(bin_id);
-      }
-      return Nneighbour;
-    }*/
-
-    inline size_t get_number_of_neighbour(){
-      return this->neighbour_ids.size();
-    }
-
-    inline  int get_neighbour_index(int j_index){
-      return this->neighbour_bin_ids[j_index].get_index();
-    }
-
-    inline  std::vector<AtomRef_t> get_centers(){
-      return this->centers;
-    }
-    void set_neighbour_ids(){
-      for (auto bin_id:this->neighbour_bin_ids){
-        
-        size_t Ncenter = this->manager.get_nb_of_center_in_box(bin_id);
-        for (auto center : centers){
-          neighbour_ids.push_back(AtomRef_t(this->manager,center.get_index()));
-        }
-        
-      }
-    }
-
-  protected:
-    Vec3i_t coordinates;
-    std::vector<AtomRef_t> centers;
-    NeighbourhoodManagerBase<NeighbourhoodManagerCell> & manager;
-    std::vector<Vec3i_t,Eigen::aligned_allocator<Vec3i_t>> neighbour_bin_shift;
-    std::vector<Dim_t> neighbour_bin_ids;
-    // TODO replace neighbour_ids by an iterator that iterates over the centers in the neighbouring boxes
-    std::vector<AtomRef_t> neighbour_ids; 
-  };
 
 
   //----------------------------------------------------------------------------//
