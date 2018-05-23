7e1c45f964c4ab02fcc93f2a53d4dc38bb885a55
felixmusil <felix.musil@epfl.ch>
2018-05-23 23:51:06 +0200

    Suppress warnings from boost. commented #define BOOST_TEST_DYN_LINK because it is  set as a compile argument in BoostTest.cmake.

--- cmake/BoostTest.cmake
+++ cmake/BoostTest.cmake
@@ -7,14 +7,6 @@ download_external_project(BoostTest
   )
 
 
-set(Boost_NO_SYSTEM_PATHS ON)
-
-include(CTest)
-enable_testing()
-
-include(CheckCXXCompilerFlag)
-include(CheckIncludeFileCXX)
-
 set(BOOST_TEST_ROOT_DIR ${_BoostTest_external_dir}/BoostTest)
 get_filename_component(BOOST_TEST_ROOT_DIR_ABS ${_BoostTest_external_dir}/BoostTest ABSOLUTE)
 
@@ -56,17 +48,21 @@ set(BOOST_UTF_SRC
     ${BOOST_TEST_ROOT_DIR}/src/xml_report_formatter.cpp
     )
 
-#message(STATUS "Boost Test  " ${BOOST_UTF_HEADERS} ${BOOST_UTF_SRC})
-
 add_library(boost_test_framework STATIC ${BOOST_UTF_HEADERS} ${BOOST_UTF_SRC})
 
 target_compile_definitions(boost_test_framework PUBLIC "-DBOOST_TEST_DYN_LINK=0")
-target_include_directories(boost_test_framework PUBLIC ${BOOST_TEST_ROOT_DIR}/include/)
+
+if (SUPPRESS_LIBRARY_WARNINGS)
+  target_include_directories(boost_test_framework SYSTEM PUBLIC ${BOOST_TEST_ROOT_DIR}/include/)
+  message(STATUS "Suppressing warnings from library Boost Unit Test Framework")
+else (SUPPRESS_LIBRARY_WARNINGS)
+  target_include_directories(boost_test_framework PUBLIC ${BOOST_TEST_ROOT_DIR}/include/)
+endif (SUPPRESS_LIBRARY_WARNINGS)
 
 set(BOOST_TEST_INCLUDE_DIR ${BOOST_TEST_ROOT_DIR}/include/)
 
 set_target_properties(boost_test_framework PROPERTIES FOLDER "UTF")
 
-message(STATUS "Boost Test" ${Boost_FOUND})
+message(STATUS "Boost Unit Test Framework " ${_BoostTest_version})
 
 
