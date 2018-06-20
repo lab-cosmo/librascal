9957d0d52ed8c972218baffc0e9f4a20523e6eca
Markus Stricker <mail@markusstricker.de>
2018-06-20 14:12:37 +0200

    test case for adaptor maxlevel

---
+++ tests/test_adaptor_increase_maxlevel.cc
@@ -0,0 +1,49 @@
+/**
+ * file   test_adaptor_increase_maxlevel.cc
+ *
+ * @author Markus Stricker <markus.stricker@epfl.ch>
+ *
+ * @date   20 Jun 2018
+ *
+ * @brief tests the implementation of the adaptor increase maxlevel
+ * (atom list to pairs, pairs to triplets, etc.)
+ *
+ * Copyright © 2018 Markus Stricker, COSMO (EPFL), LAMMM (EPFL)
+ *
+ * librascal is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3, or (at
+ * your option) any later version.
+ *
+ * librascal is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Emacs; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "tests.hh"
+#include "test_neighbourhood.hh"
+#include "neighbourhood_managers/adaptor_increase_maxlevel.hh"
+
+
+namespace rascal {
+
+  BOOST_AUTO_TEST_SUITE(maxlevel_increase_adaptor_test);
+
+  /* ---------------------------------------------------------------------- */
+  BOOST_FIXTURE_TEST_CASE(constructor_test,
+                          ManagerFixture<NeighbourhoodManagerCell>) {
+    double cut_off{cutoff_max/2};
+    AdaptorMaxLevel<NeighbourhoodManagerCell> adaptor{manager, cut_off};
+    adaptor.update();
+  }
+
+
+  BOOST_AUTO_TEST_SUITE_END();
+
+}  // rascal
