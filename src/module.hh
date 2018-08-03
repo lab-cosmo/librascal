0ce12b4fb57a5424faf5637f025607fa8295a179
gibertif <federico.giberti@gmail.com>
2018-08-03 13:38:54 +0200

    Removed useless tests, cleaned variables, added docs

--- src/module.hh
+++
@@ -1,9 +0,0 @@
-namespace rascal {
-  /**
-   * This is just a stub, but is a good illustration on howto put comments in a C++ file
-   * that can be used by Doxygen to construct a developer manual.
-   * This file contains just a function that return 2! Not bad, ah?
-   */
-
-  int f(int);
-}  // rascal
--- tests/python_binding_tests.py
+++ tests/python_binding_tests.py
@@ -2,7 +2,6 @@
 
 import unittest
 
-from python_cdist_test import TestCdist
 from python_neighbour_manager_test import TestNeighbourManagerCell
 
 class SimpleCheck(unittest.TestCase):
