5ff33b1e89d638047c8c5136935d623e5684407b
Markus Stricker <mail@markusstricker.de>
2018-08-06 14:25:18 +0200

    added cluster_ref_base as untemplated class

---
+++ src/neighbourhood_managers/cluster_ref_base.hh
@@ -0,0 +1,63 @@
+/**
+ * file   cluster_ref_base.hh
+ *
+ * @author Markus Stricker <markus.stricker@epfl.ch>
+ *
+ * @date   06 Aug 2018
+ *
+ * @brief implementation of non-templated base class for ClusterRef
+ *
+ * Copyright © 2018 Markus Stricker, COSMO (EPFL), LAMMM (EPFL)
+ *
+ * rascal is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3, or (at
+ * your option) any later version.
+ *
+ * rascal is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Emacs; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
+#ifndef CLUSTERREFBASE_H
+#define CLUSTERREFBASE_H
+
+namespace rascal {
+
+  class ClusterRefBase
+  {
+  public:
+    //! Default constructor
+    ClusterRefBase() = delete;
+
+    //! Copy constructor
+    ClusterRefBase(const ClusterRefBase & other) = delete;
+
+    //! Move constructor
+    ClusterRefBase(ClusterRefBase && other) = default;
+
+    //! Destructor
+    virtual ~ClusterRefBase() = default;
+
+    //! Copy assignment operator
+    ClusterRefBase & operator=(const ClusterRefBase & other) = delete;
+
+    //! Move assignment operator
+    ClusterRefBase & operator=(ClusterRefBase && other) = default;
+
+
+
+  protected:
+  private:
+  };
+  
+} // rascal
+
+#endif CLUSTERREFBASE_H
