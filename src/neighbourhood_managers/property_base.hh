6e526472c39541a99cabc741e376ffd4a9b424ca
Markus Stricker <mail@markusstricker.de>
2018-08-06 14:24:20 +0200

    formatting in PropertyBase

--- src/neighbourhood_managers/property_base.hh
+++ src/neighbourhood_managers/property_base.hh
@@ -42,19 +42,19 @@ namespace rascal {
     PropertyBase() = delete;
 
     //! Copy constructor
-    PropertyBase(const PropertyBase &other) = delete;
+    PropertyBase(const PropertyBase & other) = delete;
 
     //! Move constructor
-    PropertyBase(PropertyBase &&other) = default;
+    PropertyBase(PropertyBase && other) = default;
 
     //! Destructor
     virtual ~PropertyBase() = default;
 
     //! Copy assignment operator
-    PropertyBase& operator=(const PropertyBase &other) = delete;
+    PropertyBase & operator=(const PropertyBase & other) = delete;
 
     //! Move assignment operator
-    PropertyBase& operator=(PropertyBase &&other) = default;
+    PropertyBase & operator=(PropertyBase && other) = default;
 
     //! return runtime info about the stored (e.g., numerical) type
     virtual std::type_info & get_type_info() = 0;
