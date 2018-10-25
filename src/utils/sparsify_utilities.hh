f976187d64f0478be1c1ea88af629e1df67fa8cc
Markus Stricker <mail@markusstricker.de>
2018-10-25 11:25:52 +0200

    doc cleanup in src/utils

--- src/utils/sparsify_utilities.hh
+++ src/utils/sparsify_utilities.hh
@@ -34,13 +34,39 @@ namespace rascal {
   namespace utils {
     using RowMatrixXd = Eigen::Matrix<double, Eigen::Dynamic,
                                       Eigen::Dynamic, Eigen::RowMajor>;
-
-    //! interface to select features
+    /**
+     * Farthest Point Sampling selection of points given the feature matrix
+     *
+     * @param feature_matrix is a NxD matrix containing N inputs with D features
+     *        each. Defaults to numpy row-major type
+     * @param n_sparse specifies how many points should be selected. The default
+     *        value of zero sorts the whole set of inputs
+     * @param i_first_point indicates the index of the first FPS selection.
+     *        Defaults to zero
+     * @return a tuple containing the list of n_sparse indices of the selected
+     *        points, and a list of the maximum minimum distance obtained at
+     *        each stage
+     */
     std::tuple<Eigen::ArrayXi, Eigen::ArrayXd>
     select_fps(const Eigen::Ref<const RowMatrixXd> & feature_matrix,
                int n_sparse=0, int i_first_point=0);
 
-    //! interface to feature with voronoi tesselation
+    /**
+     * Farthest Point Sampling selection of points given the feature
+     * matrix. Uses a Voronoi cell algorithm that can be faster when selecting
+     * many points, or the dimensionality is relatively low.
+     *
+     * @param feature_matrix is a NxD matrix containing N inputs with D features
+     *        each. Defaults to numpy row-major type
+     * @param n_sparse specifies how many points should be selected. The default
+     *        value of zero sorts the whole set of inputs
+     * @param i_first_point indicates the index of the first FPS selection.
+     *        Defaults to zero
+     * @return a tuple containing the list of n_sparse indices of the selected
+     *        points, a list of the maximum minimum distance obtained at each
+     *        stage, a list of the assignment of each of the input points to the
+     *        FPS selected inputs, and the radius of each Voronoi cell
+     */
     std::tuple<Eigen::ArrayXi, Eigen::ArrayXd, Eigen::ArrayXi, Eigen::ArrayXd>
     select_fps_voronoi(const Eigen::Ref<const RowMatrixXd> & feature_matrix,
                        int n_sparse=0, int i_first_point=0);
