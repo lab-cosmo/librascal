39632bfb05b24948b14f3a11ccb87f5b75c52331
Max Veit <maxdveit@gmail.com>
2018-11-20 14:58:40 +0100

    Somehow missed some merge junk

--- tests/python_representation_manager_test.py
+++ tests/python_representation_manager_test.py
@@ -54,31 +54,7 @@ class TestSortedCoulombRepresentation(unittest.TestCase):
         rc.RepresentationManager.SortedCoulomb_Strict_NeighbourList_Centers(
                         self.manager,self.central_decay,self.interaction_cutoff,
                         self.interaction_decay,self.size)
-<<<<<<< HEAD
 
-    # def test_compute(self):
-        # """
-        # TEST compute wrapper
-        # """
-        # cm = rc.RepresentationManager.SortedCoulomb_Strict_NeighbourList_Centers(
-                        # self.manager,self.central_decay,self.interaction_cutoff,
-                        # self.interaction_decay,self.size)
-        # cm.compute()
-
-    # def test_get_representation(self):
-        # """
-        # TEST compute wrapper
-        # """
-        # cm = rc.RepresentationManager.SortedCoulomb_Strict_NeighbourList_Centers(
-                        # self.manager,self.central_decay,self.interaction_cutoff,
-                        # self.interaction_decay,self.size)
-
-        # cm.compute()
-
-        # rep = cm.get_representation_full()
-
-=======
-    
     # def test_compute(self):
     #     """
     #     TEST compute wrapper
@@ -100,4 +76,3 @@ class TestSortedCoulombRepresentation(unittest.TestCase):
     
     #     rep = cm.get_representation_full()
         
->>>>>>> feat/representation_manager
