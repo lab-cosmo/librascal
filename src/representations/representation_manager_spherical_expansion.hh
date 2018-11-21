98230f802a49f6c80351f07fe34484e87866dab8
Max Veit <maxdveit@gmail.com>
2018-11-21 20:16:26 +0100

    Preliminary reorganization of spherical harmonics code
    This includes moving many of the specialized mathematical and
    initialization routines into their own functions.  The issue of how to
    decide the atomic Gaussian widths at runtime remains unresolved.

--- src/representations/representation_manager_spherical_expansion.hh
+++ src/representations/representation_manager_spherical_expansion.hh
@@ -42,8 +42,9 @@
 #include "math/math_utils.hh"
 #include <algorithm>
 #include <cmath>
-#include <Eigen/Eigenvalues>
+#include <exception>
 #include <vector>
+#include <Eigen/Eigenvalues>
 
 namespace rascal {
 
@@ -51,11 +52,16 @@ namespace rascal {
 
   }
 
-  enum RadialBasisType {
-    GaussianType = 0;
-    PolynomialBasis = 1;
-  }
-
+  /**
+   * Handles the expansion of an environment in a spherical and radial basis.
+   *
+   * The local environment of each atom is represented by Gaussians of a
+   * certain width (user-defined; can be constant, species-dependent, or
+   * radially dependent).  This density field is expanded in an angular basis
+   * of spherical harmonics (à la SOAP) and a radial basis of either Gaussians
+   * (again, as in SOAP) or one of the more recent bases currently under
+   * development.
+   */
   template<class StructureManager>
   class RepresentationManagerSphericalExpansion:
     public RepresentationManagerBase
@@ -66,72 +72,42 @@ namespace rascal {
     using Property_t = Property<double, 1, 1, Eigen::Dynamic, Eigen::Dynamic>;
     using Manager_t = StructureManager;
 
-    //! Default constructor
+    /** Describes the radial basis options.  Analytical integration is
+     *  available for the Gaussian-type basis; the polynomial basis is
+     *  numerically evaluated but better conditioned.
+     */
+    enum RadialBasisType {
+      GaussianType = 0;
+      PolynomialBasis = 1;
+    };
+
+    /** Describes how the Gaussian sigma (width) is specified.  Options
+     *  are constant, species-dependent, or radially-dependent.
+     */
+    enum GaussianSigmaType {
+      Constant = 0;
+      PerSpecies = 1;
+      Radial = 2;
+    };
+
+    //! Default constructor.  Assumes constant Gaussian sigma (width).
     RepresentationManagerSphericalExpansion(Manager_t &sm, 
       double interaction_cutoff, double cutoff_smooth_width,
-      size_t n_species, size_t max_radial, size_t max_angular)
+      size_t n_species, size_t max_radial, size_t max_angular,
+      double gaussian_sigma)
       :structure_manager{sm}, interaction_cutoff{interaction_cutoff},
       cutoff_smooth_width{cutoff_smooth_width},
       max_radial{max_radial}, max_angular{max_angular},
-      n_species{n_species}, soap_vectors{sm}
+      n_species{n_species}, soap_vectors{sm},
+      constant_gaussian_sigma{gaussian_sigma}, gaussian_sigma_type{Constant}
       {
-        size_t radial_n;
-        size_t radial_n1;
-        size_t radial_n2;
-
+        this->radial_ortho_matrix.resize(this->max_radial, this->max_radial);
         this->radial_sigmas.resize(this->max_radial, 1);
-        for (radial_n = 0; radial_n < max_radial; radial_n += 1) {
-          this->radial_sigmas[radial_n] = std::max(
-                std::sqrt((float)radial_n), 1.0)
-              * interaction_cutoff / (float)max_angular;
-        }
-
-        // Precompute common prefactors
-        prefactors.resize(this->max_radial, 1);
-        for (radial_n = 0; radial_n < this->max_radial; radial_n++) {
-            this->prefactors(radial_n) = std::sqrt(
-              2.0 / std::tgamma(1.5 + radial_n)
-              * std::pow(this->radial_sigmas[radial_n], 3.0 * 2.0*radial_n));
-        }
-
-        // Compute the radial overlap matrix for later orthogonalization
-        //radial_ortho_matrix.resize(this->max_radial, this->max_radial);
-        Eigen::MatrixXd overlap(this->max_radial, this->max_radial);
-        for (radial_n1 = 0; radial_n1 < this->max_radial; radial_n1++) {
-          for (radial_n2 = 0; radial_n2 < this->max_radial; radial_n2++) {
-            overlap(radial_n1, radial_n2) =
-                std::pow(0.5/std::pow(this->radial_sigmas[radial_n1], 2)
-                       + 0.5/std::pow(this->radial_sigmas[radial_n2], 2),
-                         -0.5*(3.0 + radial_n1 + radial_n2))
-                / (std::pow(this->radial_sigmas[radial_n1], radial_n1) +
-                   std::pow(this->radial_sigmas[radial_n2], radial_n2))
-                * std::tgamma(0.5 * (3.0 + radial_n1 + radial_n2))
-                / std::pow(this->radial_sigmas[radial_n1] *
-                           this->radial_sigmas[radial_n2], 1.5)
-                * std::sqrt(std::tgamma(1.5 + radial_n1)
-                          * std::tgamma(1.5 + radial_n2));
-          }
-        }
-
-        // Compute the inverse square root of the overlap matrix
-        Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver(overlap);
-        if (eigensolver.info() != Eigen::Success) {
-          throw std::runtime_error("Warning: Could not diagonalize "
-                                   "radial overlap matrix.");
-          // And in a constructor, no less.  Maybe we should move this work
-          // somewhere else, like in a "precompute" method?
-          // Add this line: @throws runtime_error
-          //                if the overlap matrix cannot be diagonalized.
-        }
-        eigenvalues = eigensolver.eigenvalues();
-        Eigen::VectorXd eigs_invsqrt(this->max_radial);
-        for (radial_n = 0; radial_n < this->max_radial; radial_n++) {
-          eigs_invsqrt(radial_n) = 1.0 / std::sqrt(eigenvalues(radial_n));
-        }
-        unitary = eigensolver.eigenvectors();
-        //TODO check whether this is the right way to do this in Eigen
-        radial_ortho_matrix = unitary.adjoint() * eigs_invsqrt.asDiagonal()
-                              * unitary;
+        this->prefactors.resize(this->max_radial, 1);
+        // TODO(max-veit) the type of Gaussian sigma should not be determined
+        // using constructor overloading.  Better convert
+        // RepManSphExpn::get_gaussian_sigma() to its own class let that switch
+        // on the Gaussian sigma type
       }
 
     RepresentationManagerSphericalExpansion(Manager_t &sm,const hypers_t& hyper)
@@ -164,7 +140,17 @@ namespace rascal {
     //! compute representation
     void compute();
 
-    //! getter for the representation
+    //! Precompute radial Gaussian widths (NB specific to basis!)
+    void precompute_radial_sigmas();
+
+    //! Precompute radial orthogonalization matrix (NB specific to basis!)
+    void precompute_radial_overlap();
+
+    //! Compute a full set of spherical harmonics for the direction vector
+    Eigen::MatrixXd<Eigen::Dynamic, Eigen::Dynamic>
+        compute_spherical_harmonics(Eigen::Vector3d direction);
+
+    //! getter for the representation TODO(max-veit) update
     Eigen::Map<Eigen::MatrixXd> get_representation_full() {
       auto Nb_centers{this->structure_manager.nb_clusters(1)};
       auto Nb_features{this->size};
@@ -185,7 +171,9 @@ namespace rascal {
     //hypers_t hyperparmeters;
     double interaction_cutoff;
     double cutoff_smooth_width;
-    // TODO these are specific to the radial Gaussian basis
+    double constant_gaussian_sigma;
+    GaussianSigmaType gaussian_sigma_type;
+    // TODO(max-veit) these are specific to the radial Gaussian basis
     Eigen::MatrixXd<Eigen::Dynamic, 1> radial_sigmas;
     Eigen::MatrixXd<Eigen::Dynamic, 1> prefactors;
     Eigen::MatrixXd<Eigen::Dynamic, Eigen::Dynamic> radial_ortho_matrix;
@@ -200,14 +188,150 @@ namespace rascal {
   };
 
 
+  /** Compute the widths of the radial Gaussian basis functions */
+  template<class Mngr>
+  void RepresentationManagerSphericalExpansion<Mngr>::
+      precompute_radial_sigmas(){
+
+    using math::PI;
+    using math::SQRT_TWO;
+    using std::pow;
+
+    size_t radial_n;
+
+    for (radial_n = 0; radial_n < max_radial; radial_n += 1) {
+      this->radial_sigmas[radial_n] = std::max(
+            std::sqrt((double)radial_n), 1.0)
+          * this->interaction_cutoff / (double)max_angular;
+    }
+
+    // Precompute common prefactors
+    for (radial_n = 0; radial_n < this->max_radial; radial_n++) {
+      this->prefactors(radial_n) = std::sqrt(
+        2.0 / std::tgamma(1.5 + radial_n)
+        * pow(this->radial_sigmas[radial_n], 3.0 * 2.0*radial_n));
+    }
+  }
+
+  /** Compute the radial overlap matrix for later orthogonalization.
+   *
+   *  @throws runtime_error if the overlap matrix cannot be diagonalized
+   */
+  template<class Mngr>
+  void RepresentationManagerSphericalExpansion<Mngr>::
+      precompute_radial_overlap(){
+
+    using std::pow;
+    using std::sqrt;
+    using gamma = std::tgamma;
+
+    size_t radial_n1;
+    size_t radial_n2;
+
+    Eigen::MatrixXd overlap(this->max_radial, this->max_radial);
+    for (radial_n1 = 0; radial_n1 < this->max_radial; radial_n1++) {
+      for (radial_n2 = 0; radial_n2 < this->max_radial; radial_n2++) {
+        overlap(radial_n1, radial_n2) =
+            pow(0.5/pow(this->radial_sigmas[radial_n1], 2)
+                   + 0.5/pow(this->radial_sigmas[radial_n2], 2),
+                -0.5*(3.0 + radial_n1 + radial_n2))
+            / (pow(this->radial_sigmas[radial_n1], radial_n1) +
+               pow(this->radial_sigmas[radial_n2], radial_n2))
+            * gamma(0.5 * (3.0 + radial_n1 + radial_n2))
+            / pow(this->radial_sigmas[radial_n1] *
+                       this->radial_sigmas[radial_n2], 1.5)
+            * sqrt(gamma(1.5 + radial_n1)
+                 * gamma(1.5 + radial_n2));
+      }
+    }
+
+    // Compute the inverse square root of the overlap matrix
+    Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver(overlap);
+    if (eigensolver.info() != Eigen::Success) {
+      throw std::runtime_error("Warning: Could not diagonalize "
+                               "radial overlap matrix.");
+      // Add this line: @throws runtime_error
+      //                if the overlap matrix cannot be diagonalized.
+    }
+    //TODO(max-veit) check whether this is the right way to do this in Eigen
+    Eigen::MatrixXd eigenvalues = eigensolver.eigenvalues();
+    Eigen::ArrayXd eigs_invsqrt(this->max_radial) =
+        eigenvalues.array().sqrt().inverse();
+    Eigen::MatrixXd unitary = eigensolver.eigenvectors();
+    this->radial_ortho_matrix = unitary.adjoint() *
+                                eigs_invsqrt.matrix().asDiagonal() * unitary;
+  }
+
+
+  template<class Mngr>
+  void RepresentationManagerSphericalExpansion<Mngr>::
+      compute_spherical_harmonics() {
+
+    using math::PI;
+    using std::pow;
+    using std::sqrt;
+
+    size_t radial_n;
+    size_t angular_l;
+
+    // The cosine against the z-axis is just the z-component of the
+    // direction vector
+    double cos_theta = direction[2];
+    double phi = std::atan2(direction[1], direction[0]);
+    // TODO(max-veit) (at some point): Research multiple-angle formulae and
+    // see if they're more efficient.  The lines below compute cos(phi) and
+    // sin(phi) very efficiently, but we need cos(m*phi) and sin(m*phi)...
+    //double inv_sqrt_xy = 1.0 / std::sqrt(pow(direction[0], 2) +
+                                         //pow(direction[1], 2));
+    //double cos_phi = direction[0] * inv_sqrt_xy;
+    //double sin_phi = direction[1] * inv_sqrt_xy;
+    MatrixXd harmonics(max_angular+1, 2*max_angular + 1);
+    // Associated Legendre polynomials include the Condon-Shortley phase
+    // TODO(max-veit) use the recurrence relations to compute P_l^m(cos theta)
+    MatrixXd assoc_legendre_polynom(max_angular+1, 2*max_angular + 1);
+    for(angular_l = 0; angular_l < this->max_angular + 1; angular_l++) {
+      size_t m_array_idx = 0;
+      for (m_count = -1.0*angular_l; m_count < 1; m_count++) {
+        if (m_count == 0) {
+          harmonics(angular_l, m_array_idx) = assoc_legendre_polynom(
+              angular_l, m_count);
+        } else {
+          // Calculate ((l + m)! / (l - m)!) prefactor
+          size_t factorial_quotient = 1;
+          for (size_t accum = (angular_l - m_count);
+              accum < (angular_l + m_count + 1); accum++) {
+            if (accum > 0) {
+              factorial_quotient *= accum;
+            }
+          }
+          harmonics(angular_l, m_array_idx) = assoc_legendre_polynom(
+              angular_l, m_count) * -1.0 * std::sin(m_count * phi)
+              * sqrt(2.0 * (2.0*angular_l + 1.0) / (4.0 * PI));
+          harmonics(angular_l, m_array_idx - 2*m_count) =
+              assoc_legendre_polynom(angular_l, m_count)
+              * std::cos(m_count * phi)
+              * factorial_quotient
+              * sqrt(2.0 * (2.0*angular_l + 1.0) / (4.0 * PI));
+          if ((m_count % 2) == 1) {
+            // Factor of (-1)^m that goes with negative-m entries
+            harmonics(angular_l, m_array_idx - 2*m_count) *= -1.0;
+          }
+        } // if (m_count == 0)
+      } // for (m_count in [-l, 0])
+    } // for (l in [0, n))
+    return harmonics;
+  } // compute_spherical_harmonics()
+
+
   template<class Mngr>
   void RepresentationManagerSphericalExpansion<Mngr>::compute(){
 
-    using namespace rascal::math;
+    using math::PI;
+    using std::pow;
 
     this->soap_vectors.resize_to_zero();
     this->soap_vectors.set_nb_row(this->n_species * this->max_radial);
-    this->soap_vectors.set_nb_col(std::pow(this->max_angular + 1, 2));
+    this->soap_vectors.set_nb_col(pow(this->max_angular + 1, 2));
 
     size_t radial_n;
     size_t angular_l;
@@ -217,77 +341,34 @@ namespace rascal {
 
       Eigen::MatrixXd soap_vector = Eigen::MatrixXd::Zero(
           this->n_species * this->max_radial,
-          std::pow(this->max_angular + 1, 2));
+          pow(this->max_angular + 1, 2));
 
       // Start the accumulator with the central atom
       // All terms where l =/= 0 cancel
-      double sigma2 = std::pow(this->get_gaussian_sigma(), 2);
+      double sigma2 = pow(this->get_gaussian_sigma(), 2);
       for (radial_n = 0; radial_n < this->max_radial; radial_n++) {
-        // TODO remember to update when we do multiple n_species!
+        // TODO(max-veit) remember to update when we do multiple n_species!
         soap_vector(radial_n, 0) = prefactors(radial_n)
             * std::exp2(-0.5 * (1.0 - radial_n))
-            * std::pow(1.0/sigma2 + 1.0/this->radial_sigmas[radial_n]**2,
+            * pow(1.0/sigma2 + 1.0/this->radial_sigmas[radial_n]**2,
                        -0.5 * (3.0 + radial_n))
             * std::tgamma(0.5 * (3.0 + radial_n))
             * prefactors(radial_n) / std::sqrt(4.0 * PI);
       }
-      // TODO don't forget to orthogonalize!
+      // TODO(max-veit) don't forget to orthogonalize!
 
       for (auto neigh : center) {
         auto dist{this->structure_manager.get_distance(neigh)};
         auto direction{this->structure_manager.get_direction_vector(neigh)};
 
         // Precompute exponential factor
-        double exp_factor = std::exp(-0.5 * std::pow(
+        double exp_factor = std::exp(-0.5 * pow(
               dist / this->get_gaussian_sigma(neigh), 2));
 
-        // Precompute spherical harmonics TODO maths seems a little low-level
-        // for here, move into its own function?
-        // The cosine against the z-axis is just the z-component of the
-        // direction vector
-        double cos_theta = direction[2];
-        double phi = std::atan2(direction[1], direction[0]);
-        // TODO (at some point): Research multiple-angle formulae and see
-        // if they're more efficient.  The lines below compute cos(phi) and
-        // sin(phi) very efficiently, but we need cos(m*phi) and sin(m*phi)...
-        //double inv_sqrt_xy = 1.0 / std::sqrt(std::pow(direction[0], 2) +
-                                             //std::pow(direction[1], 2));
-        //double cos_phi = direction[0] * inv_sqrt_xy;
-        //double sin_phi = direction[1] * inv_sqrt_xy;
-        MatrixXd harmonics(max_angular+1, 2*max_angular + 1);
-        // Associated Legendre polynomials include the Condon-Shortley phase
-        // TODO use the recurrence relations to compute P_l^m(cos theta)
-        MatrixXd assoc_legendre_polynom(max_angular+1, 2*max_angular + 1);
-        for(angular_l = 0; angular_l < this->max_angular + 1; angular_l++) {
-          size_t m_array_idx = 0;
-          for (m_count = -1.0*angular_l; m_count < 1; m_count++) {
-            if (m_count == 0) {
-              harmonics(angular_l, m_array_idx) = assoc_legendre_polynom(
-                  angular_l, m_count);
-            } else {
-              // Calculate ((l + m)! / (l - m)!) prefactor
-              size_t factorial_quotient = 1;
-              for (size_t accum = (angular_l - m_count); accum < (angular_l + m_count + 1); accum++) {
-                if (accum > 0) {
-                  factorial_quotient *= accum;
-                }
-              }
-              harmonics(angular_l, m_array_idx) = assoc_legendre_polynom(
-                  angular_l, m_count) * -1.0 * std::sin(m_count * phi)
-                  * std::sqrt(2.0 * (2.0*angular_l + 1.0) / (4.0 * PI));
-              harmonics(angular_l, m_array_idx - 2*m_count) = assoc_legendre_polynom(
-                  angular_l, m_count) * std::cos(m_count * phi)
-                  * factorial_quotient
-                  * std::sqrt(2.0 * (2.0*angular_l + 1.0) / (4.0 * PI));
-              if ((m_count % 2) == 1) {
-                // Factor of (-1)^m that goes with negative-m entries
-                harmonics(angular_l, m_array_idx - 2*m_count) *= -1.0;
-              }
-            }
-          }
-        }
+        // Precompute spherical harmonics TODO(max-veit) maths seems a little low-level
+        MatrixXd harmonics = this->compute_spherical_harmonics(direction);
 
-        //TODO how does this work with SpeciesFilter?
+        //TODO(max-veit) how does this work with SpeciesFilter?
         for (size_t radial_n = 0; radial_n < this->max_radial; radial_n++) {
           for(size_t angular_l = 0; angular_l < this->max_angular + 1; angular_l++) {
             for (size_t idx_m = 0; idx_m < this->angular_l + 1; idx_m++) {
@@ -298,7 +379,7 @@ namespace rascal {
       } // for (neigh : center)
       this->soap_vectors.push_back(soap_vector)
     } // for (center : structure_manager)
-  }
+  } // compute()
 
   /**
    * Return the width of the atomic Gaussian for each neighbour
@@ -310,11 +391,24 @@ namespace rascal {
    * @param pair Atom pair defining the neighbour, as e.g. returned by
    *             iteration over neighbours of a centre
    *
+   * @throws logic_error if the requested sigma type has not been implemented
+   *
    */
   template<class Mngr, size_t Order, size_t Layer>
   void RepresentationManagerSphericalExpansion<Mngr>::get_gaussian_sigma(
         ClusterRefKey<Order, Layer> & pair) {
-    // return 1.0;
+    switch (this->gaussian_sigma_type) {
+    case (Constant) : {
+      return this->constant_gaussian_sigma;
+      break;
+    }
+    case (PerSpecies) :
+    case (Radial) : {
+      throw std::logic_error("Requested a sigma type that has not yet "
+                             "been implemented");
+      break;
+    }
+    }
   }
 
 }
