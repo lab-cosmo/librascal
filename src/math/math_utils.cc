5c5da53d99ed8bf914855928ee7d01805ebe647f
Max Veit <maxdveit@gmail.com>
2018-12-03 12:23:45 +0100

    Update boilerplate comments (#46) of soap-related files

--- src/math/math_utils.cc
+++ src/math/math_utils.cc
@@ -1,27 +1,28 @@
 /**
  * file   math_utils.cc
  *
+
  * @author  Felix Musil <felix.musil@epfl.ch>
+ * @author  Max Veit <max.veit@epfl.ch>
  *
  * @date   14 October 2018
  *
- * @brief contains the implementation of
- * miscellaneous math functions
+ * @brief contains the implementation of miscellaneous math functions
  *
- * Copyright © 2018  Felix Musil, COSMO (EPFL), LAMMM (EPFL)
+ * Copyright © 2018  Felix Musil, Max Veit, COSMO (EPFL), LAMMM (EPFL)
  *
  * rascal is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
+ * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3, or (at
  * your option) any later version.
  *
  * rascal is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with GNU Emacs; see the file COPYING. If not, write to the
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; see the file LICENSE. If not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
