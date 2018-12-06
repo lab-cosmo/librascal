06589aee4294ec4a03e3f1b140bcdbcdac535553
Markus Stricker <mail@markusstricker.de>
2018-12-06 09:32:57 +0100

    fixed small typo

--- bindings/bind_py_utils.cc
+++ bindings/bind_py_utils.cc
@@ -31,17 +31,17 @@
 
 
 void utils_binding(py::module & mod) {
-  py::module m_utlis_sparse = mod.def_submodule("sparsification");
-  m_utls_sps.doc() = "Sparsification Routines";
+  py::module m_utils_sparse = mod.def_submodule("sparsification");
+  m_utils_sparse.doc() = "Sparsification Routines";
 
-  m_utls_sps.def("fps", & utils::select_fps,
+  m_utils_sparse.def("fps", & utils::select_fps,
         "Selects points from a NxD dimensional"
         " feature matrix by farthest point sampling (N is the number of"
         " sample in a D dimensional space).",
         py::arg("feature_matrix"), py::arg("n_sparse"),
         py::arg("i_first_point") );
 
-  m_utls_sps.def("fps_voronoi", & utils::select_fps_voronoi,
+  m_utils_sparse.def("fps_voronoi", & utils::select_fps_voronoi,
         "Selects points from a"
         " NxD dimensional feature matrix by farthest point sampling, using"
         " a Voronoi cell method (N is the number of sample in a D dimensional"
