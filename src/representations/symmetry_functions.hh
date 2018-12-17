b160b22243ac9eedd040394bba0edddae60892d9
Markus Stricker <mail@markusstricker.de>
2018-12-17 18:29:17 +0100

    example file for Behler-Parinello and start of constructor

--- src/representations/symmetry_functions.hh
+++ src/representations/symmetry_functions.hh
@@ -2,13 +2,14 @@
  * file   symmetry_functions.hh
  *
  * @author Till Junge <till.junge@epfl.ch>
+ * @author Markus Stricker <markus.stricker@epfl.ch>
  *
  * @date   17 Dec 2018
  *
  * @brief implementation of symmetry functions for neural nets (G-functions in
  * Behler-Parinello-speak)
  *
- * Copyright © 2018 Till Junge, COSMO (EPFL), LAMMM (EPFL)
+ * Copyright © 2018 Till Junge, Markus Stricker COSMO (EPFL), LAMMM (EPFL)
  *
  * librascal is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
@@ -29,6 +30,7 @@
 #include "structure_managers/property_typed.hh"
 
 #include "Eigen/Dense"
+#include "json_io.hh"
 
 namespace rascal {
 
@@ -70,46 +72,70 @@ namespace rascal {
   };
 
   template <SymmetryFunType FunType, class StructureManager>
-  class SymmetryFunEvaluator final
-  {
+  class SymmetryFunEvaluator final {
    public:
-    using ParamStorage = Eigen::Matrix<double, FunType::NbParams, Eigen::Dynamic>;
- 
+    using ParamStorage =
+        Eigen::Matrix<double, FunType::NbParams, Eigen::Dynamic>;
+
     //! Default constructor
     SymmetryFunEvaluator() = delete;
-
 
     //! construction from json input
     SymmetryFunEvaluator(const json & hypers);
 
     //! Copy constructor
-    SymmetryFunEvaluator(const SymmetryFunEvaluator &other) = delete;
+    SymmetryFunEvaluator(const SymmetryFunEvaluator & other) = delete;
 
     //! Move constructor
-    SymmetryFunEvaluator(SymmetryFunEvaluator &&other)  = default;
+    SymmetryFunEvaluator(SymmetryFunEvaluator && other) = default;
 
     //! Destructor
     ~SymmetryFunEvaluator() = default;
 
     //! Copy assignment operator
-    SymmetryFunEvaluator& operator=(const SymmetryFunEvaluator &other) = delete;
+    SymmetryFunEvaluator &
+    operator=(const SymmetryFunEvaluator & other) = delete;
 
     //! Move assignment operator
-    SymmetryFunEvaluator& operator=(SymmetryFunEvaluator &&other) = default;
-
+    SymmetryFunEvaluator & operator=(SymmetryFunEvaluator && other) = default;
 
    protected:
     static constexpr size_t AtomOrder{1};
     static constexpr size_t PairOrder{2};
     static constexpr size_t AtomLayer{
-      std::get<0>(StructureManager::traits::LayerByOrder::type)};
+        std::get<0>(StructureManager::traits::LayerByOrder::type)};
     static constexpr size_t PairLayer{
-      std::get<1>(StructureManager::traits::LayerByOrder::type)};
+        std::get<1>(StructureManager::traits::LayerByOrder::type)};
 
     ParamStorage params;
     TypedProperty<double, AtomOrder, AtomLayer> function_values;
     TypedProperty<double, PairOrder, PairLayer> derivative_values;
+
    private:
   };
+
+  /* ---------------------------------------------------------------------- */
+  /**
+   * Constructor to size the property according to json objects content
+   */
+  template <SymmetryFunType FunType, class StructureManager>
+  SymmetryFunEvaluator<FunType, StructureManager>::SymmetryFunEvaluator(
+      const json & hypers)
+      : {
+    // get number of hyper parameters from json object
+
+    // Nested structure for similar hyper parameters? Is `hyper` the complete
+    // json input file?
+
+    // NbParams is already sized?
+    // NbParams is different, with pair or triplets
+
+    std::vector<std::vector<double>> params_tmp{
+        hypers.at("params").get<std::vector<std::vector<double>>>()};
+    size_t n_symfun{params_tmp.size()};
+
+    // possible to directly construct `params` from json or is this intermediate
+    // step necessary?
+  }
 
 }  // namespace rascal
