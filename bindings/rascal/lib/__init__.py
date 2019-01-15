65872961631571782e4b371f2042eed3137116cb
musil <felix.musil@epfl.ch>
2019-01-15 15:38:52 +0100

    fix import in the tests

--- bindings/rascal/lib/__init__.py
+++ bindings/rascal/lib/__init__.py
@@ -2,3 +2,5 @@ from ._rascal import (StructureManager, FeatureManager, Adaptor,
                      RepresentationManager, utils, math)
 
 from ._rascal.utils import sparsification
+
+import _rascal
