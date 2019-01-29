60077f8bc82a88c18bf8a09a1b1a53b00ff0161c
Max Veit <maxdveit@gmail.com>
2019-01-29 17:56:25 +0100

    Fixed two files missed in merge

--- tests/test_feature_manager.cc
+++ tests/test_feature_manager.cc
@@ -1,9 +1,5 @@
 /**
-<<<<<<< HEAD
- * file   .hh
-=======
  * file    test_feature_manager.hh
->>>>>>> origin/master
  *
  * @author Musil Felix <musil.felix@epfl.ch>
  *
@@ -13,20 +9,6 @@
  *
  * Copyright © 2018 Musil Felix, COSMO (EPFL), LAMMM (EPFL)
  *
-<<<<<<< HEAD
- * rascal is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3, or (at
- * your option) any later version.
- *
- * rascal is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GNU Emacs; see the file COPYING. If not, write to the
-=======
  * Rascal is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3, or (at
@@ -39,7 +21,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; see the file LICENSE. If not, write to the
->>>>>>> origin/master
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
@@ -52,54 +33,6 @@ namespace rascal {
 
   BOOST_AUTO_TEST_SUITE(feature_dense_test);
   /* ---------------------------------------------------------------------- */
-<<<<<<< HEAD
-  // TODO define more test that could be streamlined
-  // gets a list of fixtures for all the different possible structure managers
-  using multiple_fixtures = boost::mpl::list<
-    FeatureFixture<double, FeatureManagerDense,
-                  StructureManagerCenters,
-                  RepresentationManagerSortedCoulomb,
-                  TestFeatureData, Option::CMSortDistance>,
-    FeatureFixture<double, FeatureManagerDense,
-                  StructureManagerCenters,
-                  RepresentationManagerSortedCoulomb,
-                  TestFeatureData, Option::CMSortRowNorm>>;
-
-  BOOST_FIXTURE_TEST_CASE_TEMPLATE(multiple_setup_test,
-            Fix, multiple_fixtures, Fix) {
-  }
-
-  BOOST_FIXTURE_TEST_CASE_TEMPLATE(multiple_constructor_test,
-            Fix, multiple_fixtures, Fix) {
-    auto& features = Fix::features;
-    auto& hypers = Fix::hypers;
-    auto& Nfeature = Fix::Nfeature;
-    for (auto& hyper : hypers) {
-      features.emplace_back(Nfeature, hyper);
-    }
-
-  }
-
-  BOOST_FIXTURE_TEST_CASE_TEMPLATE(representation_aggregate_test,
-            Fix, multiple_fixtures, Fix) {
-    auto& features = Fix::features;
-    auto& hypers = Fix::hypers;
-    auto& Nfeature = Fix::Nfeature;
-    auto& Ncenter = Fix::Ncenter;
-    for (auto& hyper : hypers) {
-      features.emplace_back(Nfeature ,hyper);
-      features.back().reserve(Ncenter);
-    }
-
-    auto& representations = Fix::representations;
-    for (auto& representation : representations) {
-      features.front().push_back(representation);
-    }
-  }
-
-  BOOST_AUTO_TEST_SUITE_END();
-} // rascal
-=======
   // TODO(felix) define more test that could be streamlined
   // gets a list of fixtures for all the different possible structure managers
   using multiple_fixtures = boost::mpl::list<
@@ -191,4 +124,4 @@ namespace rascal {
   /* ---------------------------------------------------------------------- */
   BOOST_AUTO_TEST_SUITE_END();
 } // namespace rascal
->>>>>>> origin/master
+
--- tests/test_feature_manager.hh
+++ tests/test_feature_manager.hh
@@ -9,20 +9,6 @@
  *
  * Copyright © 2018 Musil Felix, COSMO (EPFL), LAMMM (EPFL)
  *
-<<<<<<< HEAD
- * rascal is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3, or (at
- * your option) any later version.
- *
- * rascal is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GNU Emacs; see the file COPYING. If not, write to the
-=======
  * Rascal is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3, or (at
@@ -35,7 +21,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; see the file LICENSE. If not, write to the
->>>>>>> origin/master
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
@@ -53,14 +38,11 @@
 
 namespace rascal {
 
-<<<<<<< HEAD
-=======
   /* ---------------------------------------------------------------------- */
   /**
    * A fixture providing access to different structures, read in from
    * json. These are used in a feature fixture to have data at hand.
    */
->>>>>>> origin/master
   struct TestFeatureData {
     TestFeatureData() = default;
     ~TestFeatureData() = default;
@@ -70,31 +52,6 @@ namespace rascal {
       "reference_data/simple_cubic_8.json",
       "reference_data/small_molecule.json"
       };
-<<<<<<< HEAD
-    std::vector<double> cutoffs{{1, 2, 3}};
-
-    std::list<json> hypers{
-      {{"central_decay", 0.5},
-      {"interaction_cutoff", 10},
-      {"interaction_decay", 0.5},
-      {"size", 120}}
-      };
-  };
-
-
-  template< typename T,
-            template<typename> class FeatureManager,
-            class StructureManager,
-            template<typename, Option ... opts > class RepresentationManager,
-            class BaseFixture,
-            Option ...options>
-  struct FeatureFixture
-  :RepresentationFixture<StructureManager, RepresentationManager,
-                          BaseFixture, options...> {
-    using Parent = RepresentationFixture<StructureManager,
-                                    RepresentationManager,
-                                    BaseFixture, options...>;
-=======
     std::vector<double> cutoffs{{1., 2., 3.}};
 
     std::list<json> hypers{
@@ -128,36 +85,10 @@ namespace rascal {
     using Parent = RepresentationFixture<StructureManager,
                                          RepresentationManager,
                                          BaseFixture>;
->>>>>>> origin/master
     using Manager_t = typename Parent::Manager_t;
     using Representation_t = typename Parent::Representation_t;
     using Feature_t = FeatureManager<T>;
     using hypers_t = typename Representation_t::hypers_t;
-<<<<<<< HEAD
-
-    FeatureFixture() :Parent{} {
-      std::vector<size_t> Nfeatures{};
-
-      auto& representations = this->representations;
-
-      for (auto& manager : this->managers_strict) {
-        for (auto& hyper : this->hypers) {
-          Ncenter += manager.size();
-
-          representations.emplace_back(manager, hyper);
-          representations.back().compute();
-          Nfeatures.push_back(representations.back().get_feature_size());
-        }
-      }
-
-      this->Nfeature =
-           *std::max_element(std::begin(Nfeatures), std::end(Nfeatures));
-    }
-
-    ~FeatureFixture() = default;
-    size_t Ncenter{0};
-    size_t Nfeature{};
-=======
     using precision_t = T;
 
     FeatureFixture() :Parent{} {
@@ -182,7 +113,6 @@ namespace rascal {
     ~FeatureFixture() = default;
     size_t n_center{0};
     size_t n_feature{};
->>>>>>> origin/master
     std::list<Feature_t> features{};
   };
 
