9dbef8516240df1a4a48dcef7f2410a74bb7db1e
Alexander Jan Goscinski <alexander.goscinski@epfl.ch>
2019-07-03 18:28:34 +0200

    fixed typos feauture; tested;

--- src/structure_managers/property_block_sparse.hh
+++ src/structure_managers/property_block_sparse.hh
@@ -574,7 +574,7 @@ namespace rascal {
 
     inline Dense_t get_dense_row(const size_t & index, const size_t & i_order) {
       auto keys = this->values[i_order][index].get_keys();
-      Dense_t feauture_row = Dense_t::Zero(this->get_nb_comp(), keys.size());
+      Dense_t feature_row = Dense_t::Zero(this->get_nb_comp(), keys.size());
       size_t i_col{0};
       for (const auto & key : keys) {
         size_t i_row{0};
@@ -585,7 +585,7 @@ namespace rascal {
         }
         i_col++;
       }
-      return feauture_row;
+      return feature_row;
     }
 
     inline Dense_t get_dense_rep() {
