61fe4f50da0edac90862ee04b6bea6794a131a8f
Alexander Jan Goscinski <alexander.goscinski@epfl.ch>
2019-08-09 09:34:50 +0200

    removed generator functions for hyp1f1 and radial contr and instead they are now initiated in the fixture;tested;

--- tests/test_math_interpolation.cc
+++ tests/test_math_interpolation.cc
@@ -92,10 +92,7 @@ namespace rascal {
         if (verbose) {
           std::cout << "Testing for n="<<n<<" and l="<<l<<std::endl;
         }
-        a = 0.5*(n+l+3);
-        b = l+1.5;
-        math::Hyp1f1 hyp1f1{a, b, 200, 1e-15};
-        func  = [&hyp1f1](double x) {return hyp1f1.calc(x);};
+        func  = [&](double x) {return this->hyp1f1.calc(x);};
         //func = std::bind(hyp1f1_function_generator, a, b, std::placeholders::_1);
         intp.initialize(func, Fix::x1, Fix::x2, Fix::mean_error_bound);
         for (int i{0}; i<ref_points.size()-1; i++) {
@@ -126,25 +123,8 @@ namespace rascal {
   // tests if functions are well approximated up to precisicion
   BOOST_FIXTURE_TEST_CASE(math_interpolator_hyp1f1_test, InterpolatorFixture<UniformInterpolator>) {
     Vector_t ref_points = Vector_t::LinSpaced(this->nb_ref_points, this->x1, this->x2);
-    int max_angular{20};
-    int max_radial{20};
-    double a,b;
-
-    bool verbose{false};
-    int l{max_angular};
-    int n{max_radial};
-    // TODO(alex) this test would take 5 seconds, therefore
-    // I only test subset
-    //for (size_t l{0}; l<max_angular; l++) {
-    //  for (size_t n{0}; n<max_radial; n++) {
-    if (verbose) {
-      std::cout << "Testing for n="<<n<<" and l="<<l<<std::endl;
-    }
-    a = 0.5*(n+l+3);
-    b = l+1.5;
-    math::Hyp1f1 hyp1f1{a, b, 200, 1e-15};
-    //std::function<double(double)> func = [&hyp1f1](double x) {return hyp1f1.calc(x);};
-    std::function<double(double)> func = std::bind(hyp1f1_function_generator, a, b, std::placeholders::_1);
+    std::function<double(double)> func = [&](double x) {return this->hyp1f1.calc(x);};
+    //std::function<double(double)> func = std::bind(hyp1f1_function_generator, a, b, std::placeholders::_1);
     intp.initialize(func, this->x1, this->x2, this->mean_error_bound); 
     BOOST_CHECK_LE(intp_ref_mean_error(this->intp, ref_points), this->mean_error_bound);
     //  }
@@ -153,11 +133,8 @@ namespace rascal {
 
   BOOST_FIXTURE_TEST_CASE(math_interpolator_radial_contribution_test, InterpolatorFixture<UniformInterpolator>) {
     Vector_t ref_points = Vector_t::LinSpaced(this->nb_ref_points, this->x1, this->x2);
-    int max_radial{3};
-    int max_angular{max_radial-1};
-    auto radial_contr{radial_contribution_generator(max_radial, max_angular)};
     std::function<double(double)> func =
-        [&radial_contr](double x) {return radial_contr.compute_contribution<rascal::internal::AtomicSmearingType::Constant>(x, 0.5)(0,0);};
+        [&](double x) {return this->radial_contr.compute_contribution<rascal::internal::AtomicSmearingType::Constant>(x, 0.5)(0,0);};
     intp.initialize(func, this->x1, this->x2, this->mean_error_bound); 
     BOOST_CHECK_LE(intp_ref_mean_error(this->intp, ref_points), this->mean_error_bound);
   }
@@ -169,25 +146,17 @@ namespace rascal {
         >;
 
   BOOST_FIXTURE_TEST_CASE(math_interpolator_vectorized_test, InterpolatorFixture<DefaultInterpolatorVectorized>) {
-      bool verbose{false};
       Vector_t ref_points = Vector_t::LinSpaced(nb_ref_points, x1, x2); 
       double error;
 
-
       // test scalar interpolator and vectorized interpolator give same results
-      int max_radial{3};
-      int max_angular{max_radial-1};
-      if (verbose) {
-        std::cout << "Testing for n="<<max_radial<<" and l="<<max_angular<<std::endl;
-      }
-      auto radial_contr{radial_contribution_generator(max_radial, max_angular)};
 
       auto intp_scalar = UniformInterpolator();
       std::function<double(double)> func_scalar =
-          [&radial_contr](double x) {return radial_contr.compute_contribution<rascal::internal::AtomicSmearingType::Constant>(x, 0.5)(0,0);};
+          [&](double x) {return this->radial_contr.compute_contribution<rascal::internal::AtomicSmearingType::Constant>(x, 0.5)(0,0);};
       intp_scalar.initialize(func_scalar, x1, x2, mean_error_bound);
 
-      std::function<Matrix_t(double)> func = [&radial_contr](double x) {return radial_contr.compute_contribution<rascal::internal::AtomicSmearingType::Constant>(x, 0.5);};
+      std::function<Matrix_t(double)> func = [&](double x) {return this->radial_contr.compute_contribution<rascal::internal::AtomicSmearingType::Constant>(x, 0.5);};
       ////// change to function generator
       ////func = std::bind(hyp1f1_function_generator, a, b, std::placeholders::_1);
       //intp.initialize(func, 0,1, mean_error_bound); 
