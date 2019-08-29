b6fde3a8437cec8ee849dc8fc16d58d08bde681e
Max Veit <maxdveit@gmail.com>
2019-08-29 19:24:00 +0200

    Update README, merge intro and index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,7 +76,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'intro'
 
 # General information about the project.
 project = 'Rascal'
