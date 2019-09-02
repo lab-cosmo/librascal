91c89333e6a8a68249ab026b2781b11cf1416a06
agoscinski <agoscinski@users.noreply.github.com>
2019-09-02 10:38:24 +0200

    fixed comment

--- scripts/generate_modified_bessel_first_kind_ref_data.py
+++ scripts/generate_modified_bessel_first_kind_ref_data.py
@@ -11,7 +11,7 @@ import json
 # Computes the sample points and weights for Gauss-Legendre quadrature and rescales them. 
 def get_leggauss(order, a, b):
     x,w = leggauss(order)
-    // rescaling
+    # rescaling
     x = (b-a)*0.5 * x + 0.5*(a+b)
     w = (b-a)*0.5 * w
     return x,w
