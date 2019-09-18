b43824f8a646295777cd5805f9b7a83508cd7e57
Guillaume Fraux <guillaume.fraux@epfl.ch>
2019-09-18 19:32:54 +0200

    Remove unreachable break

--- src/structure_managers/structure_manager_lammps.cc
+++ src/structure_managers/structure_manager_lammps.cc
@@ -82,15 +82,12 @@ namespace rascal {
     switch (order) {
     case 1: {
       return inum;
-      break;
     }
     case 2: {
       return nb_pairs;
-      break;
     }
     default:
       throw std::runtime_error("Can only handle single atoms and pairs");
-      break;
     }
   }
 }  // namespace rascal
