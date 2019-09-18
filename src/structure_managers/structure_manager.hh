f893538dda0c41761c8b87bcc06b51070812ac7f
Guillaume Fraux <guillaume.fraux@epfl.ch>
2019-09-18 19:27:23 +0200

    Remove extraneous semicolons

--- src/structure_managers/structure_manager.hh
+++ src/structure_managers/structure_manager.hh
@@ -394,7 +394,7 @@ namespace rascal {
       auto property = get_property(name);
       try {
         UserProperty_t::check_compatibility(*property);
-      } catch (const std::runtime_error & error) {
+      } catch (const std::runtime_error &) {
         return false;
       }
       return true;
@@ -1360,7 +1360,7 @@ namespace rascal {
     ProxyWithGhosts() = delete;
 
     //! Constructor
-    ProxyWithGhosts(ManagerImplementation & manager) : manager{manager} {};
+    ProxyWithGhosts(ManagerImplementation & manager) : manager{manager} {}
 
     //! Copy constructor
     ProxyWithGhosts(const ProxyWithGhosts & other) = delete;
@@ -1411,7 +1411,7 @@ namespace rascal {
     ProxyOnlyGhosts() = delete;
 
     //!
-    ProxyOnlyGhosts(ManagerImplementation & manager) : Parent{manager} {};
+    ProxyOnlyGhosts(ManagerImplementation & manager) : Parent{manager} {}
 
     //! Copy constructor
     ProxyOnlyGhosts(const ProxyOnlyGhosts & other) = delete;
