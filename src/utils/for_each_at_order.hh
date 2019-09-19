09763f6f481878baf648053c5ef8f387a3294d55
Guillaume Fraux <guillaume.fraux@epfl.ch>
2019-09-19 15:17:36 +0200

    Make cpplint happy

--- src/utils/for_each_at_order.hh
+++ src/utils/for_each_at_order.hh
@@ -66,7 +66,7 @@ namespace rascal {
                          Args &&... args) {
           function(cluster, std::forward<Args>(args)...);
         }
-    };
+      };
 
     }  // namespace detail
 
