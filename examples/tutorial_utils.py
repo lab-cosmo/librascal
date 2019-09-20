dea5de069dd09f97ca95a247bf81d492214a800f
rosecers <rosecersonsky@gmail.com>
2019-09-20 10:09:56 +0200

    Added a *very* rudimentary time estimator to tutorial notebook. Should be replaced in future instantiations

--- examples/tutorial_utils.py
+++ examples/tutorial_utils.py
@@ -13,7 +13,9 @@ from matplotlib import pyplot as plt
 import time
 from time import strftime
 from tqdm import tqdm_notebook as tqdm
-
+from threading import Thread, Timer
+from queue import Queue
+import multiprocessing
 style = {'description_width': 'initial'}
 hyper_vals = {
 # "soap_type": dict(options = ["PowerSpectrum", "RadialSpectrum"], name = "Body Order"),
@@ -78,13 +80,25 @@ def get_score(ypred,y):
                 "RMSD":[np.sqrt(np.mean((ypred-y)**2))],
                 r"$R^2$" : [get_r2(ypred, y)]
                 }
-def compute_kernel(zeta, rep1, rep2=None, kernel_type = 'global'):
+def compute_kernel(zeta, rep1, rep2=None, kernel_type = 'global', time_estimate=0):
     if(kernel_type=='atomic'):
         kernel_function = rep1.cosine_kernel_atomic
     else:
         kernel_function = rep1.cosine_kernel_global
+    # if(time_estimate!=0):
+    #     manager = multiprocessing.Manager()
+    #     return_dict = manager.dict()
+    #     # timer = Thread(target=tqdm_timer, args=(time_estimate,))
+    #     timer = multiprocessing.Process(target=tqdm_timer, args=(time_estimate,))
+    #     timer.start()
+    #     # kthread = Thread(target=kernel_function, args=(rep2, zeta,) if rep2!=None else (zeta,))
+    #     kthread = multiprocessing.Process(target=kernel_function, args=(rep2, zeta,) if rep2!=None else (zeta,))
+    #     kthread.start()
+    #     kthread.join()
+    #     print(return_dict.values())
+    #     return return_dict.values()
+    # else:
     if rep2 is not None:
-        # return [kernel_function(r, zeta) for r in rep2]
         return kernel_function(rep2, zeta)
     else:
         return kernel_function(zeta)
@@ -174,13 +188,16 @@ def _button_template_(options, description, disp_func, default=None):
     display(button)
     button.observe(disp_func, 'value')
     return button
-
+def tqdm_timer(seconds):
+    for i in tqdm(range(seconds), total=seconds):
+        time.sleep(1)
 class SOAP_tutorial(object):
     def __init__(self, input_file='./data/small_molecules-1000.xyz',training_percentage=0.8, interactive = False, verbose=True, hyperparameters = dict(**hyper_dict['Power Spectrum']), number_of_frames=None, property=None):
         self.zeta = 2
         self.Lambda = 5e-3
 
         self.hyperparameters = hyperparameters
+        self.verbose = verbose
         self.verbosity_wrap = lambda s: (None if not verbose else display(Markdown(s)))
 
         file_options = ['./data/{}'.format(f) for f in os.listdir('./data') if f.endswith('xyz')]
@@ -252,6 +269,8 @@ class SOAP_tutorial(object):
                     self.sliders[s].observe(lambda change: self.change_func(change), names='value')
         self.krr = {prop:None for prop in self.properties}
         self.trained={prop:False for prop in self.properties}
+        self.est_frames, self.est_times = [], []
+        self.pred_frames, self.pred_times = [], []
     def reset_ML(self, inp_change=False):
         if(inp_change):
             self.properties = {prop: extract_property(self.frames, prop) for prop in sorted([*list(self.frames[0].info.keys()), *list(self.frames[0].arrays.keys())], key=lambda p: -int(p in known_properties)) if prop not in ignore}
@@ -269,7 +288,9 @@ class SOAP_tutorial(object):
         change['owner'].value = change['new']
         for s in self.hyperparameters:
             if(s in self.sliders):
-                self.hyperparameters[s] = self.sliders[s].value
+                if(self.hyperparameters[s]!=self.sliders[s].value):
+                    self.est_frames, self.est_times = [], []
+                    self.hyperparameters[s] = self.sliders[s].value
         if(change['owner']==self.sliders['property_to_ml']):
             self.sliders['kernel_type'].options  =['atomic', 'global'] if self.sliders['property_to_ml'].value not in known_properties else [known_properties[self.sliders['property_to_ml'].value]]
             self.sliders['kernel_type'].value = 'atomic' if self.sliders['property_to_ml'].value not in known_properties else known_properties[self.sliders['property_to_ml'].value]
@@ -281,94 +302,112 @@ class SOAP_tutorial(object):
                 self.hyperparameters[s] = hyper_dict[self.preset_button.value][s]
                 self.sliders[s].value = self.hyperparameters[s]
         self.reset_ML()
+        # timer=Thread(target=tqdm_timer, args=(e
     def get_input(self,a):
         inp = self.input_button.value# if self.input_button.value!='Other' else input("Which file do you want to load?")
         self.input_file = inp
         self.frames = np.array(read(self.input_file, ":"))
         self.reset_ML(True)
-    def pretend_train_krr_model(self, num_frames):
-        representation = SOAP(**self.hyperparameters)
-
-        sample = np.array(np.random.randint(len(self.frames),size=num_frames))
-
-        training_properties = np.concatenate(self.properties[self.sliders['property_to_ml'].value][sample])[:,0] if known_properties[self.sliders['property_to_ml'].value]=='atomic' else self.properties[self.sliders['property_to_ml'].value][sample]
-
-        features = representation.transform(self.frames[sample])
-
-        kernel = compute_kernel(self.zeta, features, kernel_type=self.sliders['kernel_type'].value)
-        delta = np.std(training_properties) / np.mean(kernel.diagonal())
-        kernel[np.diag_indices_from(kernel)] += self.Lambda**2 / delta **2 + 1e-8
-
-        weights = np.linalg.solve(kernel,training_properties)
-        model = KRR(self.zeta, weights, representation, features, kernel_type=self.sliders['kernel_type'].value)
-
     def train_krr_model(self, jitter=1e-8):
-        self.output_params()
-        est = self.estimate_time()
-        print(est)
-        self.verbosity_wrap("<br/>We will now train a model on {}.".format(self.sliders['property_to_ml'].value))
 
-        representation = SOAP(**self.hyperparameters)
+        self.verbosity_wrap(markdown_table_from_dict({"Training Set": [int(self.sliders['number_of_frames'].value*(self.sliders['training_percentage'].value)),round(100*(self.sliders['training_percentage'].value))],
+                                                      "Testing Set":[int(self.sliders['number_of_frames'].value*(1-self.sliders['training_percentage'].value)),round(100*(1-self.sliders['training_percentage'].value))]}, headers=["Partition","Number of Frames", "Percentage"]))
 
         self.train_idx, self.test_idx = split_dataset(self.frames[:self.sliders['number_of_frames'].value],self.sliders['training_percentage'].value)
+        self.train_krr_model_func(self.train_idx, jitter=jitter)
+    def train_krr_model_func(self, frame_idx, jitter=1e-8, pretend=False):
+        if(pretend==False):
+            self.output_params()
+            verbosity_wrap = lambda s: self.verbosity_wrap(s)
+        else:
+            verbosity_wrap = lambda s: None
 
-        training_properties = np.concatenate(self.properties[self.sliders['property_to_ml'].value][self.train_idx])[:,0] if known_properties[self.sliders['property_to_ml'].value]=='atomic' else self.properties[self.sliders['property_to_ml'].value][self.train_idx]
+        verbosity_wrap("<br/>We will now train a model on {}.".format(self.sliders['property_to_ml'].value))
 
-        self.verbosity_wrap("First, I am going to separate my dataset:")
-        self.verbosity_wrap(markdown_table_from_dict({"Training Set": [len(self.train_idx),round(100*(self.sliders['training_percentage'].value))],
-                                                      "Testing Set":[len(self.test_idx),round(100*(1-self.sliders['training_percentage'].value))]}, headers=["Partition","Number of Frames", "Percentage"]))
+        representation = SOAP(**self.hyperparameters)
+
+        training_properties = np.concatenate(self.properties[self.sliders['property_to_ml'].value][frame_idx])[:,0] if known_properties[self.sliders['property_to_ml'].value]=='atomic' else self.properties[self.sliders['property_to_ml'].value][frame_idx]
+
+        verbosity_wrap("First, I am going to separate my dataset:")
+        verbosity_wrap("<br/>Now we will compute the SOAP representation of our training frames.")
+
         t = time.time()
+        features = representation.transform(self.frames[frame_idx])
 
-        self.verbosity_wrap("<br/>Now we will compute the SOAP representation of our training frames.")
+        verbosity_wrap('This took {} seconds/frame.'.format(round((time.time()-t)/len(frame_idx),8)))
 
-        features = representation.transform(self.frames[self.train_idx])
-        self.verbosity_wrap('This took {} seconds/frame.'.format(round((time.time()-t)/len(self.train_idx),8)))
+        if(pretend==False):
+            verbosity_wrap("Estimating time to compute kernel...")
+            self.estimate_time(N=max(0,20-len(self.est_frames)))
+            est = int(np.poly1d(np.polyfit(self.est_frames,self.est_times,deg=2))(len(frame_idx)))+1
+        else:
+            est=0
 
-        self.verbosity_wrap("<br/>Next we find the kernel for our training model.<br/>(This step will take approximately {} minutes and {} seconds.)".format(int(est/60), int(est%60)))
+        verbosity_wrap("<br/>Next we find the kernel for our training model.<br/>(This step will take approximately {} minutes and {} seconds.)".format(int(est/60), int(est%60)))
+        time.sleep(0.5)
+        t = time.time()
+        kernel = compute_kernel(self.zeta, features, kernel_type=self.sliders['kernel_type'].value, time_estimate=est)
+        self.est_frames.append(len(frame_idx))
+        self.est_times.append(time.time()-t)
 
-        time.sleep(.5)
-        kernel = compute_kernel(self.zeta, features, kernel_type=self.sliders['kernel_type'].value)
         delta = np.std(training_properties) / np.mean(kernel.diagonal())
         kernel[np.diag_indices_from(kernel)] += self.Lambda**2 / delta **2 + jitter
 
-        self.verbosity_wrap("<br/>We will adjust the diagonals of our kernel by {} so that it is properly scaled.".format(round(self.Lambda**2 / delta **2 + jitter,8)))
-        self.verbosity_wrap("<br/>Now we can take this kernel to compute the weights of our KRR.")
+        verbosity_wrap("<br/>We will adjust the diagonals of our kernel by {} so that it is properly scaled.".format(round(self.Lambda**2 / delta **2 + jitter,8)))
+        verbosity_wrap("<br/>Now we can take this kernel to compute the weights of our KRR.")
 
         weights = np.linalg.solve(kernel,training_properties)
         model = KRR(self.zeta, weights, representation, features, kernel_type=self.sliders['kernel_type'].value)
-        self.krr[self.sliders['property_to_ml'].value], k = model, kernel
-        self.trained[self.sliders['property_to_ml'].value] = True
+        if(pretend==False):
+            self.krr[self.sliders['property_to_ml'].value], k = model, kernel
+            self.trained[self.sliders['property_to_ml'].value] = True
     def plot_prediction(self):
+        self.plot_prediction_func(self.test_idx)
+    def plot_prediction_func(self, frame_idx, pretend=False):
+        if(pretend==False):
+            verbosity_wrap = lambda s: self.verbosity_wrap(s)
+        else:
+            verbosity_wrap = lambda s: None
         if(self.trained[self.sliders['property_to_ml'].value]==False):
-            self.verbosity_wrap("Model has not yet been trained, training now..."   )
-            self.train_model(self.sliders['property_to_ml'].value)
-        testing_properties =np.concatenate(self.properties[self.sliders['property_to_ml'].value][self.test_idx])[:,0] if known_properties[self.sliders['property_to_ml'].value]=='atomic' else self.properties[self.sliders['property_to_ml'].value][self.test_idx]
-        y_pred = self.krr[self.sliders['property_to_ml'].value].predict(self.frames[self.test_idx])
-        self.verbosity_wrap(markdown_table_from_dict(get_score(y_pred, testing_properties), headers=["Statistic","Value" ]))
+            verbosity_wrap("Model has not yet been trained, training now..."   )
+            self.train_krr_model()
+        testing_properties =np.concatenate(self.properties[self.sliders['property_to_ml'].value][frame_idx])[:,0] if self.sliders['kernel_type'].value=='atomic' else self.properties[self.sliders['property_to_ml'].value][frame_idx]
 
-        plt.scatter(y_pred, testing_properties, s=3)
-        plt.axis('scaled')
-        plt.xlabel('DFT energy / (eV/atom)')
-        plt.ylabel('Predicted energy / (eV/atom)')
-        plt.gca().set_aspect('equal')
-        plt.show()
+        if(pretend==False):
+            verbosity_wrap("Estimating time to compute prediction...")
+            self.estimate_time(x=self.pred_frames, y=self.pred_times, f=self.plot_prediction_func, N=max(0,20-len(self.pred_frames)), ref=self.sliders['number_of_frames'].value*(1-self.sliders['training_percentage'].value))
+            est = int(np.poly1d(np.polyfit(self.pred_frames,self.pred_times,deg=2))(len(frame_idx)))+1
+        else:
+            est=0
+
+        t = time.time()
+        verbosity_wrap("Predicting the properties of our test set will take approximately {} minutes and {} seconds.".format(int(est/60), int(est%60)))
+        y_pred = self.krr[self.sliders['property_to_ml'].value].predict(self.frames[frame_idx])
+        self.pred_frames.append(len(frame_idx))
+        self.pred_times.append(time.time()-t)
+        verbosity_wrap(markdown_table_from_dict(get_score(y_pred, testing_properties), headers=["Statistic","Value" ]))
+
+        if(not pretend):
+            plt.scatter(y_pred, testing_properties, s=3)
+            plt.axis('scaled')
+            plt.xlabel(self.sliders['property_to_ml'].value)
+            plt.ylabel('Predicted '+self.sliders['property_to_ml'].value)
+            plt.gca().set_aspect('equal')
+            plt.show()
     def output_params(self):
         self.verbosity_wrap('Our input file is {}, of which we are using {} frames.'.format(self.input_file, self.sliders['number_of_frames'].value))
         self.verbosity_wrap("<br/>Our hyperparameters are {}".format(markdown_table_from_dict({hyper_vals[k]['name']:[self.hyperparameters[k]] for k in self.hyperparameters if 'fixed' not in hyper_vals[k]}, headers =["Parameter", "Value"])))
     def set(self, value_name, value):
         assert value_name in self.sliders
         self.sliders[value_name].value=value
-    def estimate_time(self):
-        import time
+    def estimate_time(self, x=None, y=None, f=None, N=20, ref =None):
+        if(N<=0):
+            return
+        if(x==None or y==None or f==None or ref==None):
+            x=self.est_frames
+            y=self.est_times
+            f=self.train_krr_model_func
+            ref = int(0.25*self.sliders['number_of_frames'].value*self.sliders['training_percentage'].value)
 
-        x = []
-        y = []
-
-        self.verbosity_wrap("Estimating time to compute kernel...")
-        for nf in tqdm(np.random.randint(2,int(0.25*self.sliders['number_of_frames'].value*self.sliders['training_percentage'].value), size=min(20, int(0.25*self.sliders['number_of_frames'].value*self.sliders['training_percentage'].value)))):
-            start_time=time.time()
-            self.pretend_train_krr_model(num_frames=nf)
-            x.append(nf)
-            y.append(start_time-time.time())
-            print(x,y)
-        return np.poly1d(np.polyfit(x,y,deg=2))(self.sliders['number_of_frames'].value*self.sliders['training_percentage'].value)
+        for nf in tqdm(np.random.randint(2,ref, size=min(N, ref))):
+            f(frame_idx=np.random.randint(len(self.frames),size=nf), pretend=True)
