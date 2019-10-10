80e4728ebf4bdca06637f7c04c2a5b7c8fec4509
Alexander Jan Goscinski <alexander.goscinski@epfl.ch>
2019-10-10 20:24:28 +0200

    added header to new test files

--- tests/test_cluster_ref_key.hh
+++ tests/test_cluster_ref_key.hh
@@ -1,3 +1,30 @@
+/**
+ * @file   test_cluster_ref_key.hh
+ *
+ * @author Alexander Goscinski <alexander.goscinski@epfl.ch>
+ * @date   10 Thu 2019
+ *
+ * @brief  tests for the cluster ref key layer computation utilities
+ *
+ * Copyright  2019 Alexander Goscinski, COSMO (EPFL), LAMMM (EPFL)
+ *
+ * Rascal is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3, or (at
+ * your option) any later version.
+ *
+ * Rascal is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; see the file LICENSE. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
 #ifndef TESTS_TEST_CLUSTER_REF_KEY_HH_
 #define TESTS_TEST_CLUSTER_REF_KEY_HH_
 
