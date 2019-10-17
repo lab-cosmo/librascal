7f9f6d241349d1867bd4237e940b259ad391c479
Guillaume Fraux <guillaume.fraux@epfl.ch>
2019-10-17 13:52:40 +0200

    Update pybind11 & wigxjpf to targets

--- cmake/wigxjpf.cmake
+++ cmake/wigxjpf.cmake
@@ -2,21 +2,19 @@
 
 set(SOURCE_DIR "${_wigxjpf_external_dir}/${package}")
 
-include_directories(BEFORE SYSTEM
-${SOURCE_DIR}/inc
-${SOURCE_DIR}/cfg)
-
 download_external_project(wigxjpf
-  URL "http://fy.chalmers.se/subatom/wigxjpf/wigxjpf-${_wigxjpf_version}.tar.gz"
-  BACKEND NONE
-  THIRD_PARTY_SRC_DIR ${_wigxjpf_external_dir}
-  BUILD_COMMAND "make -C ${SOURCE_DIR}"
-  ${_wigxjpf_update}
-  )
+    URL "http://fy.chalmers.se/subatom/wigxjpf/wigxjpf-${_wigxjpf_version}.tar.gz"
+    BACKEND NONE
+    THIRD_PARTY_SRC_DIR ${_wigxjpf_external_dir}
+    BUILD_COMMAND "make -C ${SOURCE_DIR}"
+    ${_wigxjpf_update}
+)
 
-  add_library("${WIGXJPF_NAME}" STATIC IMPORTED)
+add_library("${WIGXJPF_NAME}" STATIC IMPORTED)
 
-  set_property(TARGET "${WIGXJPF_NAME}" PROPERTY IMPORTED_LOCATION ${SOURCE_DIR}/lib/libwigxjpf.a)
+set_target_properties("${WIGXJPF_NAME}" PROPERTIES
+    IMPORTED_LOCATION ${SOURCE_DIR}/lib/libwigxjpf.a
+    INTERFACE_INCLUDE_DIRECTORIES "${SOURCE_DIR}/inc;${SOURCE_DIR}/cfg"
+)
 
-  add_dependencies("${WIGXJPF_NAME}" wigxjpf)
-
+add_dependencies("${WIGXJPF_NAME}" wigxjpf)
