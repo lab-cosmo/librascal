af505a551860646e4554174af6e7961e6c586cc7
Alexander Jan Goscinski <alexander.goscinski@epfl.ch>
2019-10-17 18:19:59 +0200

    put deepclean in a general developer utils script; additional argument needed to use deepclean to prevent that accidental execution of the script leads to direct deletion of all files

--- scripts/deepclean.py
+++
@@ -1,8 +0,0 @@
-from os import listdir, remove
-from os.path import isfile, join
-from shutil import rmtree
-for f in listdir('./'):
-    if (isfile(f)):
-        remove(f)
-    elif (f != 'external') and (f != 'third-party'):
-        rmtree(f)
