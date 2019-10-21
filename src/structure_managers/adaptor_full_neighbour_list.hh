83a7178df3bcfff6bf3cad1e66a54dc950d6e98d
Guillaume Fraux <guillaume.fraux@epfl.ch>
2019-10-21 15:20:10 +0200

    Cleanup switch/case statements
    No need for a new block at the case level, and no need for
    break after return/throw

--- src/structure_managers/adaptor_full_neighbour_list.hh
+++ src/structure_managers/adaptor_full_neighbour_list.hh
@@ -142,17 +142,12 @@ namespace rascal {
     //! returns the number of atoms or pairs
     size_t get_nb_clusters(int order) const {
       switch (order) {
-      case 1: {
+      case 1:
         return this->manager->get_nb_clusters(order);
-        break;
-      }
-      case 2: {
+      case 2:
         return this->neighbours_atom_tag.size();
-        break;
-      }
-      default: {
+      default:
         throw std::runtime_error("Can only handle single atoms and pairs.");
-      }
       }
     }
 
