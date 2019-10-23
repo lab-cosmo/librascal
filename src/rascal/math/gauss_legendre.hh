ab1ad59c47128ae5991b8ba24798f98bd25fd273
Guillaume Fraux <guillaume.fraux@epfl.ch>
2019-10-23 16:37:11 +0200

    Split gauss_legendre.hh

--- src/rascal/math/gauss_legendre.hh
+++ src/rascal/math/gauss_legendre.hh
@@ -31,8 +31,6 @@
 
 #include "rascal/math/utils.hh"
 
-#include <map>
-
 namespace rascal {
   namespace math {
     /**
@@ -48,48 +46,8 @@ namespace rascal {
      * see https://github.com/JuliaApproximation/FastGaussQuadrature.jl
      * for potential improvements of the method used here (Numerical Recepies)
      */
-    inline MatrixX2_t compute_gauss_legendre_points_weights(double r_st,
-                                                            double r_nd,
-                                                            int order_n) {
-      if (order_n < 2) {
-        throw std::runtime_error(R"(there should be at least 2 integration
-        points but: order_n < 2 == true)");
-      }
-      MatrixX2_t point_weight(order_n, 2);
-
-      double z{0.}, pp{0.}, r_midpoint{(r_st + r_nd) * 0.5},
-          r_length{0.5 * (r_nd - r_st)};
-
-      int m{static_cast<int>((order_n + 1) / 2)};
-      // the weights and points are symmetric so only half need to be computed
-      for (int ii{0}; ii < m; ++ii) {
-        // starting guess for the ith root
-        z = std::cos(math::PI * (ii + 0.75) / (order_n + 0.5));
-        double z1{0.};
-        while (std::abs(z - z1) > DBL_FTOL) {
-          double p1{1.}, p2{0.};
-          // use LP 3-term recurrence relation with p1/2/3
-          for (int jj{0}; jj < order_n; ++jj) {
-            double p3{p2};
-            p2 = p1;
-            p1 = ((2.0 * jj + 1.0) * z * p2 - jj * p3) / (jj + 1);
-          }
-          // compute the derivative of the LP
-          pp = order_n * (z * p1 - p2) / (z * z - 1.0);
-          z1 = z;
-          // Newton's method
-          z = z1 - p1 / pp;
-        }
-
-        // scale the root to the [r_st, r_nd] interval
-        point_weight(ii, 0) = r_midpoint - r_length * z;
-        point_weight(order_n - 1 - ii, 0) = r_midpoint + r_length * z;
-        // compute the weight
-        point_weight(ii, 1) = (2.0 * r_length) / ((1.0 - z * z) * pp * pp);
-        point_weight(order_n - 1 - ii, 1) = point_weight(ii, 1);
-      }
-      return point_weight;
-    }
+    MatrixX2_t compute_gauss_legendre_points_weights(double r_st, double r_nd,
+                                                     int order_n);
 
   }  // namespace math
 }  // namespace rascal
