6133c340468f800f6252667639ffa492a9099dfc
musil <felix.musil@epfl.ch>
2019-11-12 14:37:31 +0100

    fix compilation errors in benchmarks

--- performance/profiles/profile_spherical_expansion.cc
+++ performance/profiles/profile_spherical_expansion.cc
@@ -135,8 +135,8 @@ int main(int argc, char * argv[]) {
             << std::endl;
 
   if (VERBOSE) {
-    auto expn = *manager->template get_property_ptr<PropGrad_t>(
-                 representation.get_gradient_name()).get_features();
+    auto expn = manager->template get_property_ptr<PropGrad_t>(
+                 representation.get_gradient_name())->get_features();
     std::cout << "Sample SphericalExpansion elements " << std::endl
               << expn(0, 0) << " " << expn(0, 1) << " " << expn(0, 2) << "\n"
               << expn(1, 0) << " " << expn(1, 1) << " " << expn(1, 2) << "\n"
@@ -161,9 +161,9 @@ int main(int argc, char * argv[]) {
             << elapsed_grad.count() / elapsed.count() << std::endl;
 
   if (VERBOSE) {
-    auto expn2 = *manager->template get_property_ptr<PropGrad_t>(
+    auto expn2 = manager->template get_property_ptr<PropGrad_t>(
                  representation_gradients.get_gradient_name())
-                 .get_features();
+                 ->get_features();
     std::cout << "Sample SphericalExpansion elements (should be identical) "
               << std::endl
               << expn2(0, 0) << " " << expn2(0, 1) << " " << expn2(0, 2) <<
