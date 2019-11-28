472edb130be6fe636cc1abb609c61a9eaaa1108e
rosecers <rosecersonsky@gmail.com>
2019-11-28 13:48:22 +0100

    Fixed test_calculator

--- tests/test_calculator.cc
+++ tests/test_calculator.cc
@@ -405,7 +405,8 @@ namespace rascal {
     using ClusterRef_t = typename Fix::Manager_t::template ClusterRef<2>;
     using RadialIntegral_t = typename Fix::RadialIntegral_t;
 
-    GradientTestFixture test_data{"reference_data/radial_derivative_test.json"};
+    GradientTestFixture test_data{"reference_data/tests_only/"
+                                  "radial_derivative_test.json"};
     auto && it_manager{managers.front()->begin()};  // Need only one manager
     auto && atom{*it_manager};
     auto && it_atom{atom.begin()};
