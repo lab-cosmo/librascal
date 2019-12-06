6d524739a0e9bee9141c7774000ed6ce405ab75f
Markus Stricker <mail@markusstricker.de>
2019-12-06 14:16:41 +0100

    comment adjusted

--- src/rascal/structure_managers/property_typed.hh
+++ src/rascal/structure_managers/property_typed.hh
@@ -319,10 +319,10 @@ namespace rascal {
      * Accessor for last pushed entry for dynamically sized properties
      */
     reference back() {
-      // -1 is correction for 0-start indexing
       if (this->values.size() == 0) {
         throw std::runtime_error("Property is empty, .back() is undefined.");
       }
+      // -1 is correction for 0-start indexing
       auto && index{this->values.size() / this->get_nb_comp() - 1};
       return Value_t::get_ref(this->values[index * this->get_nb_comp()],
                               this->get_nb_row(), this->get_nb_col());
