b4d44c6221890a2bff0eaeecc4ed610cda4cfa9c
felixmusil <felix.musil@epfl.ch>
2020-03-23 17:40:22 +0100

    add fps filter

---
+++ bindings/rascal/utils/filter_utils.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+# utility functions for Filters
+def get_index_mappings_sample_per_species(managers, sps):
+    # get various info from the structures about the center atom species and indexing
+    types = []
+    strides_by_sp = {sp: [0] for sp in sps}
+    global_counter = {sp: 0 for sp in sps}
+    indices_by_sp = {sp: [] for sp in sps}
+    map_by_manager = [{} for ii in range(len(managers))]
+    for i_man in range(len(managers)):
+        man = managers[i_man]
+        counter = {sp: 0 for sp in sps}
+        for i_at, at in enumerate(man):
+            types.append(at.atom_type)
+            if at.atom_type in sps:
+                map_by_manager[i_man][global_counter[at.atom_type]] = i_at
+                counter[at.atom_type] += 1
+                global_counter[at.atom_type] += 1
+            else:
+                raise ValueError('Atom type {} has not been specified in fselect: {}'.format(
+                    at.atom_type, sps))
+        for sp in sps:
+            strides_by_sp[sp].append(counter[sp])
+
+    for sp in sps:
+        strides_by_sp[sp] = np.cumsum(strides_by_sp[sp])
+
+    for ii, sp in enumerate(types):
+        indices_by_sp[sp].append(ii)
+
+    return strides_by_sp, global_counter, map_by_manager, indices_by_sp
+
+def get_index_mappings_sample(managers):
+    # get various info from the structures about the center atom species and indexing
+    strides = [0]
+    global_counter = 0
+    map_by_manager = [{} for ii in range(len(managers))]
+    for i_man in range(len(managers)):
+        man = managers[i_man]
+        counter = 0
+        for i_at, _ in enumerate(man):
+            map_by_manager[i_man][global_counter] = i_at
+            counter += 1
+            global_counter += 1
+        strides.append(counter)
+
+    strides = np.cumsum(strides)
+
+    return strides, global_counter, map_by_manager
+
+def convert_selected_global_index2rascal_sample_per_species(managers, selected_ids_by_sp, strides_by_sp, map_by_manager, sps):
+    # convert selected center indexing into the rascal format
+    selected_ids = [[] for ii in range(len(managers))]
+    for sp in sps:
+        ids = convert_selected_global_index2rascal_sample(managers, selected_ids_by_sp[sp], strides_by_sp[sp], map_by_manager)
+        for ii,selected_idx in zip(ids, selected_ids):
+            selected_idx.extend(ii)
+    for ii in range(len(selected_ids)):
+        selected_ids[ii] = list(np.sort(selected_ids[ii]))
+    return selected_ids
+
+def convert_selected_global_index2rascal_sample(managers, selected_ids_global, strides, map_by_manager):
+    # convert selected center indexing into the rascal format
+    selected_ids = [[] for ii in range(len(managers))]
+    i_manager = 0
+    for idx in selected_ids_global:
+        carry_on = True
+        while carry_on:
+            if idx >= strides[i_manager] and idx < strides[i_manager + 1]:
+                selected_ids[i_manager].append(map_by_manager[i_manager][idx])
+                carry_on = False
+            else:
+                i_manager += 1
+    for ii in range(len(selected_ids)):
+        selected_ids[ii] = list(np.sort(selected_ids[ii]).astype(int))
+    return selected_ids
--- bindings/rascal/utils/fps.py
+++ bindings/rascal/utils/fps.py
@@ -1,4 +1,9 @@
 from ..lib import sparsification
+# from ..models import PseudoPoints
+from .filter_utils import (get_index_mappings_sample_per_species,
+convert_selected_global_index2rascal_sample_per_species,
+get_index_mappings_sample,
+convert_selected_global_index2rascal_sample)
 import numpy as np
 
 
@@ -87,3 +92,148 @@ def fps(feature_matrix, n_select, starting_index=None,
     return_dict["fps_hausdorff_d2"] = lmin_d2
 
     return return_dict
+
+
+class FPSFilter(object):
+    """Farther Point Sampling (FPS) to select samples or features in a given feature matrix.
+    Wrapper around the fps function for convenience.
+    Parameters
+    ----------
+    representation : Calculator
+        Representation calculator associated with the kernel
+    Nselect: int
+        number of points to select. if act_on='sample per specie' then it should
+        be a dictionary mapping atom type to the number of samples, e.g.
+        Nselect = {1:200,6:100,8:50}.
+    act_on: string
+        Select how to apply the selection. Can be either of 'sample',
+        'sample per species','feature'.
+    starting_index: int
+        Index used to start the FPS selection with.
+
+    """
+
+    def __init__(self, representation, Nselect, act_on='sample per specie', starting_index=0):
+        super(FPSFilter, self).__init__()
+        self._representation = representation
+        self.Nselect = Nselect
+        self.starting_index = starting_index
+        if act_on in ['sample', 'sample per specie', 'feature']:
+            self.act_on = act_on
+        else:
+            raise 'Wrong input: {}'.format(act_on)
+
+        self.selected_ids = None
+        self.fps_minmax_d2_by_sp = None
+        self.fps_minmax_d2 = None
+
+    def fit(self, managers):
+        """Perform CUR selection of samples/features.
+        Parameters
+        ----------
+        managers : AtomsList
+            list of structures containing features computed with representation
+        Returns
+        -------
+        PseudoPoints
+            Selected samples
+        Raises
+        ------
+        ValueError
+            [description]
+        NotImplementedError
+            [description]
+        """
+
+        # get the dense feature matrix
+        X = managers.get_features(self._representation)
+
+        if self.act_on in ['sample per specie']:
+            sps = list(self.Nselect.keys())
+
+            # get various info from the structures about the center atom species and indexing
+            (strides_by_sp, global_counter, map_by_manager,
+             indices_by_sp) = get_index_mappings_sample_per_species(managers, sps)
+
+            print('The number of pseudo points selected by central atom species is: {}'.format(
+                self.Nselect))
+
+            # organize features w.r.t. central atom type
+            X_by_sp = {}
+            for sp in sps:
+                X_by_sp[sp] = X[indices_by_sp[sp]]
+            self._XX = X_by_sp
+
+            # split the dense feature matrix by center species and apply CUR decomposition
+            self.selected_ids_by_sp = {}
+            self.fps_minmax_d2_by_sp = {}
+            self.fps_hausforff_d2_by_sp = {}
+            for sp in sps:
+                print('Selecting species: {}'.format(sp))
+                fps_out = fps(X_by_sp[sp], self.Nselect[sp], starting_index=self.starting_index)
+                self.selected_ids_by_sp[sp] = fps_out['fps_indices']
+                self.fps_minmax_d2_by_sp[sp] = fps_out['fps_minmax_d2']
+
+            return self
+        elif self.act_on in ['feature']:
+            fps_out = fps(X.T, self.Nselect, starting_index=self.starting_index)
+            self.selected_ids = fps_out['fps_indices']
+            self.fps_minmax_d2 = fps_out['fps_minmax_d2']
+        elif self.act_on in ['sample']:
+            fps_out = fps(X, self.Nselect, starting_index=self.starting_index)
+            self.selected_ids_global = fps_out['fps_indices']
+            self.fps_minmax_d2 = fps_out['fps_minmax_d2']
+        else:
+            raise NotImplementedError("method: {}".format(self.act_on))
+
+    def transform(self, managers):
+        if self.act_on in ['sample per specie']:
+            sps = list(self.Nselect.keys())
+            # get various info from the structures about the center atom species and indexing
+            (strides_by_sp, global_counter, map_by_manager,
+             indices_by_sp) = get_index_mappings_sample_per_species(managers, sps)
+            selected_ids_by_sp = {key:val[:self.Nselect[key]] for key,val in self.selected_ids_by_sp.items()}
+            self.selected_ids = convert_selected_global_index2rascal_sample_per_species(
+                managers, selected_ids_by_sp, strides_by_sp, map_by_manager, sps)
+            return self.selected_ids
+            # # build the pseudo points
+            # pseudo_points = PseudoPoints(self._representation)
+            # pseudo_points.extend(managers, self.selected_ids)
+            # return pseudo_points
+        elif self.act_on in ['sample']:
+            selected_ids_global = self.selected_ids_global[:self.Nselect]
+            strides, _, map_by_manager = get_index_mappings_sample(managers)
+            self.selected_ids = convert_selected_global_index2rascal_sample(managers,
+                                                        selected_ids_global, strides, map_by_manager)
+            return self.selected_ids
+            # # build the pseudo points
+            # pseudo_points = PseudoPoints(self._representation)
+            # pseudo_points.extend(managers, self.selected_ids)
+            # return pseudo_points
+
+        elif self.act_on in ['feature']:
+            feat_idx2coeff_idx = self._representation.get_feature_index_mapping(managers)
+            selected_features = {key:[] for key in feat_idx2coeff_idx[0].keys()}
+            for idx in self.selected_ids[:self.Nselect]:
+                coef_idx = feat_idx2coeff_idx[idx]
+                for key in selected_features.keys():
+                    selected_features[key].append(int(coef_idx[key]))
+            # keep the global indices for ease of use
+            selected_features['selected_features_global_ids'] = self.selected_ids[:self.Nselect].tolist()
+            return dict(coefficient_subselection=selected_features)
+
+    def plot(self):
+        import matplotlib.pyplot as plt
+        if self.fps_minmax_d2_by_sp is None:
+            plt.semilogy(self.fps_minmax_d2,label=self.act_on)
+
+        else:
+            for sp in self.fps_minmax_d2_by_sp:
+                plt.semilogy(self.fps_minmax_d2_by_sp[sp],
+                            label='{} species {}'.format(self.act_on, sp))
+            plt.legend()
+        plt.title('FPSFilter')
+        plt.ylabel('fps minmax d^2')
+
+    def fit_transform(self, managers):
+        return self.fit(managers).transform(managers)
