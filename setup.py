eba3681a677f6e8a92a40edb1b6a9e1bc7e439fc
felixmusil <felix.musil@epfl.ch>
2020-03-29 17:07:03 +0200

    update setup and req

--- setup.py
+++ setup.py
@@ -2,18 +2,11 @@ import sys
 import os
 from os.path import join
 from distutils.sysconfig import get_python_lib
+
 from setuptools import find_packages
+from skbuild import setup
+from skbuild.constants import CMAKE_INSTALL_DIR
 
-try:
-    from skbuild import setup
-    from skbuild.constants import CMAKE_INSTALL_DIR
-
-except ImportError:
-    print('scikit-build is required to build from source.', file=sys.stderr)
-    print('Please run:', file=sys.stderr)
-    print('', file=sys.stderr)
-    print('  python3 -m pip install scikit-build')
-    sys.exit(1)
 
 with open('./requirements_pip.txt', 'r') as fp:
     requirements = list(filter(lambda x: '#' not in x, (line.strip() for line in fp)))
