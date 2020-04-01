a1c66e1e14a3fd7fce486aa10a10cf7021991601
felixmusil <felix.musil@epfl.ch>
2020-04-01 19:44:03 +0200

    refactor baseIO

--- bindings/rascal/utils/io.py
+++ bindings/rascal/utils/io.py
@@ -4,6 +4,7 @@ from collections import Iterable
 import numpy as np
 import json
 from copy import deepcopy
+from abc import ABC, abstractmethod
 
 BETA_VERSION = '0.1'
 
@@ -48,24 +49,6 @@ def load_obj(fn):
 
     """
     return BaseIO().from_file(fn)
-
-
-def from_dict(data):
-    """Convert a python dictionary created by the obj.to_dict() function
-    defined in BaseIO
-
-    Parameters
-    ----------
-    data : dictionary
-
-    Returns
-    -------
-    object
-        the full object described by data
-    """
-    # make a deepcopy to avoid problems with in place modifications of data
-    # happening in from_dict()
-    return BaseIO().from_dict(deepcopy(data))
 
 
 def dump_json(fn, data):
@@ -223,142 +206,144 @@ def get_supported_io_versions():
     return list(dict2obj.keys())
 
 
-class BaseIO(object):
+class BaseIO(ABC):
     """
-    Base class giving an arbitrary python class the ability to serialize
-    themselves into python dictionary and being saved to files.
-    A class that inherits from BaseIO should implement the _get_data and get_init_params functions (see _get_state() for more details).
-    """
+    fetch the state of self using the _get_data and get_init_params functions that should be implemented in the Implementation class that inherits from BaseIO.
 
-    def __init__(self):
-        super(BaseIO, self).__init__()
+    get_init_params is expected to return a dictionary containing all the
+    parameters used by the __init__() methods.
 
-    def _get_state(self):
-        """
-        fetch the state of self using the _get_data and get_init_params functions that should be implemented in the Implementation class that inherits from BaseIO.
+    _get_data is expected to return a dictionary containing all the data
+    that is not set by the initialization of the class."""
+    @abstractmethod
+    def _get_data(self):
+        pass
 
-        get_init_params is expected to return a dictionary containing all the
-        parameters used by the __init__() methods.
+    @abstractmethod
+    def get_init_params(self):
+        pass
 
-        _get_data is expected to return a dictionary containing all the data
-        that is not set by the initialization of the class."""
-        state = dict(data=self._get_data(),
-                     init_params=self.get_init_params())
-        return state
+def _get_state(obj):
+    if isinstance(obj, BaseIO) is True:
+        state = dict(data=obj._get_data(),
+                 init_params=obj.get_init_params())
+    else:
+        raise ValueError(
+                'input object: "{}" does not inherit from "BaseIO"'.format(obj))
+    return state
 
-    def to_dict(self, obj=None, version=CURRENT_VERSION):
-        """recursirvely convert the python object obj into a dictionary that
-        can be used to create a copy of obj.
-        obj has to inherit from BaseIO and implements a _get_data and
-        get_init_params methods"""
-        if obj is None:
-            obj = self
-        state = obj._get_state()
+def to_dict(obj, version=CURRENT_VERSION):
+    """recursirvely convert the python object obj into a dictionary that
+    can be used to create a copy of obj.
+    obj has to inherit from BaseIO and implements a _get_data and
+    get_init_params methods"""
 
-        for name, entry in state.items():
-            if isinstance(entry, dict):
-                for k, v in entry.items():
-                    if isinstance(v, BaseIO) is True:
-                        state[name][k] = self.to_dict(v, version)
-                    elif isinstance(v, list):
-                        ll = []
-                        for val in v:
-                            if isinstance(val, BaseIO) is True:
-                                ll.append(self.to_dict(val, version))
-                            else:
-                                ll.append(val)
-                        state[name][k] = ll
+    state = _get_state(obj)
 
-        data = obj2dict[version](obj.__class__, state)
-        return data
+    for name, entry in state.items():
+        if isinstance(entry, dict):
+            for k, v in entry.items():
+                if isinstance(v, BaseIO) is True:
+                    state[name][k] = to_dict(v, version)
+                elif isinstance(v, list):
+                    ll = []
+                    for val in v:
+                        if isinstance(val, BaseIO) is True:
+                            ll.append(to_dict(val, version))
+                        else:
+                            ll.append(val)
+                    state[name][k] = ll
 
-    def from_dict(self, data):
-        """recursirvely convert python a dictionary that
-        obj has to inherit from BaseIO."""
+    data = obj2dict[version](obj.__class__, state)
+    return data
+
+def from_dict(data):
+    """recursirvely convert python a dictionary that
+    obj has to inherit from BaseIO."""
+    version = data['version']
+    for name, entry in data.items():
+        if isinstance(entry, dict):
+            for k, v in entry.items():
+                if is_valid_object_dict[version](v) is True:
+                    data[name][k] = from_dict(v)
+                elif isinstance(v, list):
+                    ll = []
+                    for val in v:
+                        if is_valid_object_dict[version](val) is True:
+                            ll.append(from_dict(val))
+                        else:
+                            ll.append(val)
+
+                    data[name][k] = ll
+
+    obj = dict2obj[version](data)
+    return obj
+
+def to_file(fn, obj, version=CURRENT_VERSION):
+    """Saves the object to a file name 'fn' using the to_dict()
+        serialization procedure."""
+    fn = os.path.abspath(fn)
+    filename, file_extension = os.path.splitext(fn)
+    data = to_dict(obj, version=version)
+    class_name = data['class_name'].lower()
+
+    if file_extension == '.json':
+        _dump_npy(fn, data, class_name)
+        dump_json(fn, data)
+    else:
+        raise NotImplementedError(
+            'Unknown file extention: {}'.format(file_extension))
+
+def from_file(fn):
+    """Loads an object that was saved using to_file() from a file"""
+    fn = os.path.abspath(fn)
+    path = os.path.dirname(fn)
+    filename, file_extension = os.path.splitext(fn)
+    if file_extension == '.json':
+        data = load_json(fn)
         version = data['version']
-        for name, entry in data.items():
-            if isinstance(entry, dict):
-                for k, v in entry.items():
-                    if is_valid_object_dict[version](v) is True:
-                        data[name][k] = self.from_dict(v)
-                    elif isinstance(v, list):
-                        ll = []
-                        for val in v:
-                            if is_valid_object_dict[version](val) is True:
-                                ll.append(self.from_dict(val))
-                            else:
-                                ll.append(val)
-
-                        data[name][k] = ll
-
-        obj = dict2obj[version](data)
-        return obj
-
-    def to_file(self, fn, version=CURRENT_VERSION):
-        """Saves the object to a file name 'fn' using the to_dict()
-         serialization procedure."""
-        fn = os.path.abspath(fn)
-        filename, file_extension = os.path.splitext(fn)
-        data = self.to_dict(version=version)
-        class_name = data['class_name'].lower()
-
-        if file_extension == '.json':
-            self._dump_npy(fn, data, class_name)
-            dump_json(fn, data)
+        if is_valid_object_dict[version](data):
+            _load_npy(data, path)
+            return from_dict(data)
         else:
-            raise NotImplementedError(
-                'Unknown file extention: {}'.format(file_extension))
+            raise RuntimeError(
+                'The file: {}; does not contain a valid dictionary representation of an object.'.format(fn))
 
-    def from_file(self, fn):
-        """Loads an object that was saved using to_file() from a file"""
-        fn = os.path.abspath(fn)
-        path = os.path.dirname(fn)
-        filename, file_extension = os.path.splitext(fn)
-        if file_extension == '.json':
-            data = load_json(fn)
-            version = data['version']
-            if is_valid_object_dict[version](data):
-                self._load_npy(data, path)
-                return self.from_dict(data)
-            else:
-                raise RuntimeError(
-                    'The file: {}; does not contain a valid dictionary representation of an object.'.format(fn))
+    else:
+        raise NotImplementedError(
+            'Unknown file extention: {}'.format(file_extension))
 
-        else:
-            raise NotImplementedError(
-                'Unknown file extention: {}'.format(file_extension))
+def _dump_npy(fn, data, class_name):
+    """Saves numpy array that are large into a file different from the
+    main saved file. The main file contains a relative path to the *.npy
+    file so that it can be loaded properly.
+    Small numpy array are converted to lists and saved in the main file."""
+    filename, file_extension = os.path.splitext(fn)
+    for k, v in data.items():
+        if isinstance(v, dict):
+            if 'class_name' in data:
+                class_name = data['class_name'].lower()
+            _dump_npy(fn, v, class_name)
+        elif is_large_array(v) is True:
+            if 'tag' in data:
+                class_name += '-' + data['tag']
+            v_fn = filename + '-{}-{}'.format(class_name, k) + '.npy'
+            v_bfn = os.path.basename(v_fn)
+            data[k] = v_bfn
+            np.save(v_fn, v)
 
-    def _dump_npy(self, fn, data, class_name):
-        """Saves numpy array that are large into a file different from the
-        main saved file. The main file contains a relative path to the *.npy
-        file so that it can be loaded properly.
-        Small numpy array are converted to lists and saved in the main file."""
-        filename, file_extension = os.path.splitext(fn)
-        for k, v in data.items():
-            if isinstance(v, dict):
-                if 'class_name' in data:
-                    class_name = data['class_name'].lower()
-                self._dump_npy(fn, v, class_name)
-            elif is_large_array(v) is True:
-                if 'tag' in data:
-                    class_name += '-' + data['tag']
-                v_fn = filename + '-{}-{}'.format(class_name, k) + '.npy'
-                v_bfn = os.path.basename(v_fn)
-                data[k] = v_bfn
-                np.save(v_fn, v)
+        elif is_npy(v) is True:
+            data[k] = ['npy', v.tolist()]
 
-            elif is_npy(v) is True:
-                data[k] = ['npy', v.tolist()]
-
-    def _load_npy(self, data, path):
-        """Loads a numpy array saved using _dump_npy(). The array is mmaped
-        so it is physically loaded only when needed."""
-        for k, v in data.items():
-            if isinstance(v, dict):
-                self._load_npy(v, path)
-            elif is_npy_filename(v) is True:
-                data[k] = np.load(os.path.join(path, v), mmap_mode='r')
-            elif isinstance(v, list):
-                if len(v) == 2:
-                    if 'npy' == v[0]:
-                        data[k] = np.array(v[1])
+def _load_npy(data, path):
+    """Loads a numpy array saved using _dump_npy(). The array is mmaped
+    so it is physically loaded only when needed."""
+    for k, v in data.items():
+        if isinstance(v, dict):
+            _load_npy(v, path)
+        elif is_npy_filename(v) is True:
+            data[k] = np.load(os.path.join(path, v), mmap_mode='r')
+        elif isinstance(v, list):
+            if len(v) == 2:
+                if 'npy' == v[0]:
+                    data[k] = np.array(v[1])
