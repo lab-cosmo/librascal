bd1b90050cd1b3f7471527c9458cac99f96c0350
felixmusil <felix.musil@epfl.ch>
2020-04-03 17:20:12 +0200

    add scorer utils

--- bindings/rascal/utils/__init__.py
+++ bindings/rascal/utils/__init__.py
@@ -5,7 +5,9 @@ from copy import deepcopy
 from .io import (BaseIO, to_dict, from_dict, get_current_io_version,
                  get_supported_io_versions, dump_obj, load_obj)
 from .cur import CURFilter
+from .scorer import get_score, print_score
 
+# function to redirect c++'s standard output to python's one
 ostream_redirect = utils.__dict__['ostream_redirect']
 
 
