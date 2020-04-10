32eba2f87da45cf9fc98e65c8fefb55e399c82ac
felixmusil <felix.musil@epfl.ch>
2020-04-10 12:20:47 +0200

    fix bug in filter utils

--- bindings/rascal/utils/filter_utils.py
+++ bindings/rascal/utils/filter_utils.py
@@ -9,14 +9,14 @@ def get_index_mappings_sample_per_species(managers, sps):
     strides_by_sp = {sp: [0] for sp in sps}
     global_counter = {sp: 0 for sp in sps}
     indices_by_sp = {sp: [] for sp in sps}
-    map_by_manager = [{} for ii in range(len(managers))]
+    map_by_manager = {sp:[{} for ii in range(len(managers)) ] for sp in sps}
     for i_man in range(len(managers)):
         man = managers[i_man]
         counter = {sp: 0 for sp in sps}
         for i_at, at in enumerate(man):
             types.append(at.atom_type)
             if at.atom_type in sps:
-                map_by_manager[i_man][global_counter[at.atom_type]] = i_at
+                map_by_manager[at.atom_type][i_man][global_counter[at.atom_type]] = i_at
                 counter[at.atom_type] += 1
                 global_counter[at.atom_type] += 1
             else:
@@ -33,7 +33,6 @@ def get_index_mappings_sample_per_species(managers, sps):
         indices_by_sp[sp].append(ii)
 
     return strides_by_sp, global_counter, map_by_manager, indices_by_sp
-
 
 def get_index_mappings_sample(managers):
     # get various info from the structures about the center atom species and indexing
@@ -54,20 +53,6 @@ def get_index_mappings_sample(managers):
     return strides, global_counter, map_by_manager
 
 
-def convert_selected_global_index2rascal_sample_per_species(managers, selected_ids_by_sp,
-                                strides_by_sp, map_by_manager, sps):
-    # convert selected center indexing into the rascal format
-    selected_ids = [[] for ii in range(len(managers))]
-    for sp in sps:
-        ids = convert_selected_global_index2rascal_sample(
-            managers, selected_ids_by_sp[sp], strides_by_sp[sp], map_by_manager)
-        for ii, selected_idx in zip(ids, selected_ids):
-            selected_idx.extend(ii)
-    for ii in range(len(selected_ids)):
-        selected_ids[ii] = list(np.sort(selected_ids[ii]))
-    return selected_ids
-
-
 def convert_selected_global_index2rascal_sample(managers, selected_ids_global,
                                                         strides, map_by_manager):
     # convert selected center indexing into the rascal format
@@ -83,4 +68,18 @@ def convert_selected_global_index2rascal_sample(managers, selected_ids_global,
                 i_manager += 1
     for ii in range(len(selected_ids)):
         selected_ids[ii] = list(np.sort(selected_ids[ii]).astype(int))
+    return selected_ids
+
+
+def convert_selected_global_index2rascal_sample_per_species(managers, selected_ids_by_sp,
+                                strides_by_sp, map_by_manager, sps):
+    # convert selected center indexing into the rascal format
+    selected_ids = [[] for ii in range(len(managers))]
+    for sp in sps:
+        ids = convert_selected_global_index2rascal_sample(
+            managers, selected_ids_by_sp[sp], strides_by_sp[sp], map_by_manager[sp])
+        for ii, selected_idx in zip(ids, selected_ids):
+            selected_idx.extend(ii)
+    for ii in range(len(selected_ids)):
+        selected_ids[ii] = list(np.sort(selected_ids[ii]))
     return selected_ids
