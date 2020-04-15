1c45ea9f4d994f7c3f7b237ca10e68dc525ea903
felixmusil <felix.musil@epfl.ch>
2020-04-15 17:25:28 +0200

    set version in package

--- setup.py
+++ setup.py
@@ -6,14 +6,24 @@ from distutils.sysconfig import get_python_lib
 from setuptools import find_packages
 from skbuild import setup
 from skbuild.constants import CMAKE_INSTALL_DIR
+import re
 
+# read the version number from the library
+pattern = r"[0-9]\.[0-9]\.[0-9]"
+version = None
+with open('./bindings/rascal/__init__.py', 'r') as fp:
+    for line in fp.readlines():
+        if '__version__' in line:
+            version = re.findall(pattern, line)[0]
+if version is None:
+    raise ValueError("Version number not found.")
 
 with open('./requirements_pip.txt', 'r') as fp:
     requirements = list(filter(lambda x: '#' not in x, (line.strip() for line in fp)))
 
 setup(
     name="rascal",
-    version="0.3.9",
+    version=version,
     description="""A versatile and scalable computation of representations of
 atomic structures for machine learning.""",
     author='Félix Musil',
