a560394048504f71e5e297bbcc53c6875700e1df
Félix <felix.musil@epfl.ch>
2020-05-22 12:27:09 +0200

    switch to black formater

--- cmake/black.cmake
+++ cmake/black.cmake
@@ -1,21 +1,3 @@
-# ******************************************************************************
-# Reference:
-# https://github.com/NervanaSystems/ngraph/blob/master/cmake/Modules/style_check.cmake
-#
-# Copyright 2017-2019 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ******************************************************************************
 
 set(BLACK_TARGET pretty-python CACHE STRING "Name of python autoformatter")
 
@@ -42,9 +24,6 @@ else()
       " https://github.com/psf/black")
     set(BLACK_FOUND FALSE)
 endif()
-
-
-
 
 # use black.py to check source code files inside DIR directory
 function(black_add_subdirectory DIR)
