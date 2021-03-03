68d382f7ec8a1039790668a58dcb5a352adf3d71
andrea.grisafi <andrea.grisafi91@gmail.com>
2021-03-03 19:02:24 +0100

    LODE installation debugged

--- bindings/rascal/representations/base.py
+++ bindings/rascal/representations/base.py
@@ -8,7 +8,7 @@ import queue
 # Register Calculators
 _representations_list = ["sortedcoulomb", "sphericalexpansion", 
                          "sphericalinvariants", "sphericalcovariants",
-                         "sphericalexpansionkspace", "sphericalinvariantskspace"]
+                         "kspacesphericalexpansion", "kspacesphericalinvariants"]
 _representations = {}
 for k, v in representation_calculators.__dict__.items():
     if "pybind11_builtins.pybind11_type" in str(type(v)):
