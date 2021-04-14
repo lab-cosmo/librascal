5b274070fa36cebeeed5a5330974e48cca977222
rosecers <rosecersonsky@gmail.com>
2021-04-14 16:36:49 +0200

    Moving fps and cur to scikit-cosmo version with backwards compatibility
    * Adding skcosmo to requirements
    
    * Make scikit-cosmo an optional dependency (and add to CI)

---
+++ tests/python/test_filter.py
@@ -0,0 +1,10 @@
+import unittest
+
+from bindings.rascal.utils import FPSFilter, CURFilter
+
+class CURTest(unittest.TestCase):
+    def test_one(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
