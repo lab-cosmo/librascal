37e5d82f724fce27d585fa0759624be5f8b41dc0
alexgo <alex.goscinski@posteo.de>
2021-11-30 10:01:17 +0100

    make pretty

--- src/rascal/representations/calculator_spherical_covariants.hh
+++ src/rascal/representations/calculator_spherical_covariants.hh
@@ -292,8 +292,9 @@ namespace rascal {
 
     int get_num_coefficients(int /*n_species*/) const {
       std::stringstream error{};
-      error << "CalculatorSphericalCovariants.get_num_coefficients: Function 'get_num_coefficients' is not implemented "
-            << "(file " << __FILE__ <<", line " << __LINE__ << ")."
+      error << "CalculatorSphericalCovariants.get_num_coefficients: Function "
+               "'get_num_coefficients' is not implemented "
+            << "(file " << __FILE__ << ", line " << __LINE__ << ")."
             << std::endl;
       throw std::runtime_error(error.str());
     }
