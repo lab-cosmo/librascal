94e1ec75cca15672434500e5a34101c53ee4bcca
alexgo <alex.goscinski@posteo.de>
2021-12-08 19:08:38 +0100

    clean setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
 import sys
-import os
 from os.path import join
 from distutils.sysconfig import get_python_lib
-
 from setuptools import find_packages
+from pkg_resources import parse_requirements
 from skbuild import setup
 from skbuild.constants import CMAKE_INSTALL_DIR
-import re
+
 from bindings.librascal import __version__
 
 with open("AUTHORS.txt") as fp:
     author = ", ".join(fp.readlines()).replace("\n", "").replace("\n", "")
 
-from pkg_resources import parse_requirements
 
 with open("./requirements/minimal.txt", "r") as fp:
     install_requires = [
         str(requirement) for requirement in parse_requirements(fp.read())
     ]
-
-from pathlib import Path
-
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
 
 with open("README.rst") as f:
     long_description = f.read()
