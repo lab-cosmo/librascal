aa96cae37be69bd4f3403bac317c4c17a37eb183
Francois Berenger <berenger@edu.k.u-tokyo.ac.jp>
2022-06-07 10:47:13 +0200

    soap_test.py: state of my current trial at dumping SOAP atom features

--- soap_test.py
+++ soap_test.py
@@ -1,41 +1,73 @@
 #!/usr/bin/env python3
 
-# Try to dump SOAP vectors for each atom of a molecule
+# Try to dump SOAP vectors (features) for each atom of each molecule
 
+import sys
+import numpy as np
 from ase.io import read as ase_read
 from rascal.representations import SphericalInvariants as SOAP
 
-max_radial = 6 # What is this integer? Unit? Reasonable range to explore?
-max_angular = 6 # What is this integer? Unit? Reasonable range to explore?
-soap_type = "PowerSpectrum"  # only physicists know what is a power spectrum...
-#            "RadialSpectrum" # the other possibility; also only known to physicists...
+# What is this integer? Unit? Reasonable range to explore?
+max_radial = 6
+# What is this integer? Unit? Reasonable range to explore?
+max_angular = 6
+# what is a power spectrum?
+soap_type = "PowerSpectrum"
+#           "RadialSpectrum" # only other possibility
 if soap_type == "RadialSpectrum":
     max_angular = 0
 
 input_fn = "reference_data/inputs/small_molecules-20.json"
 
-start = 0
-length = 1
-representation_name = "spherical_invariants" # Any other choice?
-target_type = "Atom" # The other choice is Structure; i.e. granularity is per atom or per molecule
+# only those atomic numbers in input_fn
+global_species = [1,6,7,8]
+
+# doc can be found at
+# https://github.com/lab-cosmo/librascal/bindings/rascal/representations/
+#   spherical_invariants.py
 
 hyper_params = {
-    "interaction_cutoff": 3.5, # Angstroms? what is the reasonable range to explore for this value?
-    "cutoff_smooth_width": 0.5, # Angstroms? what is the reasonable range to explore for this value?
+    # Angstroms? what is the reasonable range to explore for this value?
+    "interaction_cutoff": 3.5,
+    # Angstroms? what is the reasonable range to explore for this value?
+    "cutoff_smooth_width": 0.5,
     "max_radial": max_radial,
     "max_angular": max_angular,
-    "gaussian_sigma_type": "Constant", # other possible choices for this value?
-    "gaussian_sigma_constant": 0.5, # Angstroms? what is the reasonable range to explore for this value?
+    "gaussian_sigma_type": "Constant", # only possible choice
+    # Angstroms? what is the reasonable range to explore for this value?
+    "gaussian_sigma_constant": 0.5,
     "soap_type": soap_type,
-    "cutoff_function_type": "ShiftedCosine", # other possible choices for this value?
-    "normalize": True,
-    "radial_basis": "GTO", # other possible choices for this value?
+    # 'RadialScaling' is the only other choice
+    "cutoff_function_type": "ShiftedCosine",
+    "normalize": True, # recommended
+    # 'DVR' is the only other choice
+    "radial_basis": "GTO",
+    # # as far as I can tell, global_species has no effect on fixing the
+    # # dimensionality of the SOAP feature vectors
+    # "global_species": global_species
 }
 soap = SOAP(**hyper_params)
 
-molecules = ase_read(input_fn, "{}:{}".format(start, start + length))
-soap_encoded_molecules = soap.transform(molecules)
+read_all = ":" # ASE constant meaning "read all molecules"
+molecules = ase_read(input_fn, read_all)
 
-for molecule in soap_encoded_molecules:
-    for soap_encoded_atom in molecule:
-        print(soap_encoded_atom)
+# FBR: we cannot put too many molecules at once in memory; or it will start
+#      swapping; the problem is that SOAP features are high dimensional
+
+for molecule in molecules:
+    m = molecule
+    m.cell = [200,200,200] # big enough unit cell
+    m.positions[:] += 100 # center atoms in this unit cell
+    m.pbc = [False, False, False] # disable periodic boundary conditions
+    soap_encoded_molecule = soap.transform(m)
+    # n_atoms x n_features
+    soap_np_array = soap_encoded_molecule.get_features(soap)
+    nb_atoms = soap_np_array.shape[0]
+    nb_features = soap_np_array.shape[1]
+    print("Data matrix: (%d, %d)" % soap_np_array.shape, file=sys.stderr)
+    # dump each atom's index; followed by its SOAP features
+    for i_atom in range(nb_atoms):
+        print('%d' % i_atom, end='')
+        for j_feat in range(nb_features):
+            print(' %d:%f' % (j_feat, soap_np_array[i_atom][j_feat]), end='')
+        print()
