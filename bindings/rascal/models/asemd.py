7161a9a8eba7bcc3f7152dc0a034dd16a336d0ff
DavideTisi <davide.tisi93@gmail.com>
2023-02-14 22:21:06 +0100

    linting

--- bindings/rascal/models/asemd.py
+++ bindings/rascal/models/asemd.py
@@ -29,11 +29,13 @@ class ASEMLCalculator(Calculator, BaseIO):
         if not isinstance(model, list):
             model = [model]
 
-        if not isinstance(representation,list):
+        if not isinstance(representation, list):
             representation = [representation]
 
-        if len(model)!=len(representation):
-            raise ValueError('should provide a representation per model. len(model)!=len(representation)')
+        if len(model) != len(representation):
+            raise ValueError(
+                "should provide a representation per model. len(model)!=len(representation)"
+            )
 
         self.model = model
         self.representation = representation
@@ -52,7 +54,7 @@ class ASEMLCalculator(Calculator, BaseIO):
             #  happens at the begining of the MD run
             at = self.atoms.copy()
             at.wrap(eps=1e-11)
-            self.manager = [at]*len(self.model)
+            self.manager = [at] * len(self.model)
         elif isinstance(self.manager, list) and isinstance(self.manager[0], AtomsList):
             structure = unpack_ase(self.atoms, wrap_pos=True)
             structure.pop("center_atoms_mask")
@@ -65,14 +67,22 @@ class ASEMLCalculator(Calculator, BaseIO):
             energy += self.model[i].predict(self.manager[i])
             if "forces" in properties:
                 if i == 0:
-                    self.results["forces"] = self.model[i].predict_forces(self.manager[i])
+                    self.results["forces"] = self.model[i].predict_forces(
+                        self.manager[i]
+                    )
                 else:
-                    self.results["forces"] += self.model[i].predict_forces(self.manager[i])
+                    self.results["forces"] += self.model[i].predict_forces(
+                        self.manager[i]
+                    )
             if "stress" in properties:
                 if i == 0:
-                    self.results["stress"] = self.model[i].predict_stress(self.manager[i]).flatten()
+                    self.results["stress"] = (
+                        self.model[i].predict_stress(self.manager[i]).flatten()
+                    )
                 else:
-                    self.results["stress"] += self.model[i].predict_stress(self.manager[i]).flatten()
+                    self.results["stress"] += (
+                        self.model[i].predict_stress(self.manager[i]).flatten()
+                    )
 
         self.results["energy"] = energy
         self.results["free_energy"] = energy
