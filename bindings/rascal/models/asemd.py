79755c7e80812b9b22037764f707306370f5e519
DavideTisi <davide.tisi93@gmail.com>
2023-02-14 21:30:26 +0100

    added support for sum of potentials

--- bindings/rascal/models/asemd.py
+++ bindings/rascal/models/asemd.py
@@ -26,6 +26,15 @@ class ASEMLCalculator(Calculator, BaseIO):
 
     def __init__(self, model, representation, **kwargs):
         super(ASEMLCalculator, self).__init__(**kwargs)
+        if not isinstance(model, list):
+            model = [model]
+
+        if not isinstance(representation,list):
+            representation = [representation]
+
+        if len(model)!=len(representation):
+            raise ValueError('should provide a representation per model. len(model)!=len(representation)')
+
         self.model = model
         self.representation = representation
         self.kwargs = kwargs
@@ -43,24 +52,33 @@ class ASEMLCalculator(Calculator, BaseIO):
             #  happens at the begining of the MD run
             at = self.atoms.copy()
             at.wrap(eps=1e-11)
-            self.manager = [at]
-        elif isinstance(self.manager, AtomsList):
+            self.manager = [at]*len(self.model)
+        elif isinstance(self.manager, list) and isinstance(self.manager[0], AtomsList):
             structure = unpack_ase(self.atoms, wrap_pos=True)
             structure.pop("center_atoms_mask")
-            self.manager[0].update(**structure)
+            for i in range(len(self.manager)):
+                self.manager[i][0].update(**structure)
 
-        self.manager = self.representation.transform(self.manager)
+        energy = 0
+        for i in range(len(self.manager)):
+            self.manager[i] = self.representation[i].transform(self.manager[i])
+            energy += self.model[i].predict(self.manager[i])
+            if "forces" in properties:
+                if i == 0:
+                    self.results["forces"] = self.model[i].predict_forces(self.manager[i])
+                else:
+                    self.results["forces"] += self.model[i].predict_forces(self.manager[i])
+            if "stress" in properties:
+                if i == 0:
+                    self.results["stress"] = self.model[i].predict_stress(self.manager[i]).flatten()
+                else:
+                    self.results["stress"] += self.model[i].predict_stress(self.manager[i]).flatten()
 
-        energy = self.model.predict(self.manager)
         self.results["energy"] = energy
         self.results["free_energy"] = energy
-        if "forces" in properties:
-            self.results["forces"] = self.model.predict_forces(self.manager)
-        if "stress" in properties:
-            self.results["stress"] = self.model.predict_stress(self.manager).flatten()
 
     def _get_init_params(self):
-        init_params = dict(model=self.model, representation=self.representation)
+        init_params = dict(model=self.model[0], representation=self.representation[0])
         init_params.update(**self.kwargs)
         return init_params
 
